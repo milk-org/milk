6423768396fbbfc066f075dc08c13afd6647691b
oguyon <oliv.guyon@gmail.com>
2017-11-24 11:45:24 -1000

    msync

--- src/COREMOD_tools/COREMOD_tools.c
+++ src/COREMOD_tools/COREMOD_tools.c
@@ -169,7 +169,8 @@ void __attribute__ ((constructor)) libinit_COREMOD_tools()
 int init_COREMOD_tools()
 {
     strcpy(data.module[data.NBmodule].name, __FILE__);
-    strcpy(data.module[data.NBmodule].info, "milk    - Image information and statistics");
+    strcpy(data.module[data.NBmodule].package, "milk");
+    strcpy(data.module[data.NBmodule].info, "Image information and statistics");
     data.NBmodule++;
 
 
