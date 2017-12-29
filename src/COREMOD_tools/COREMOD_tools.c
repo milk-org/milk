05ff8019fb196220adb37309351e70f7d144cb95
oguyon <oliv.guyon@gmail.com>
2017-12-28 22:02:05 -1000

    msync

--- src/COREMOD_tools/COREMOD_tools.c
+++ src/COREMOD_tools/COREMOD_tools.c
@@ -160,7 +160,13 @@ int_fast8_t COREMOD_TOOLS_statusStat_cli()
 void __attribute__ ((constructor)) libinit_COREMOD_tools()
 {
 	init_COREMOD_tools();
-//	printf(" ...... Loading module %s\n", __FILE__);
+	
+	if(data.progStatus>0)
+	{
+		printf("  Found unloaded shared object in ./libs/ -> LOADING module %s\n", __FILE__);
+		fflush(stdout);
+	}	
+
 }
 
 
