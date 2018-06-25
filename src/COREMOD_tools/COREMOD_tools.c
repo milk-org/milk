6202147984a9c879a2389983a679b95af8b77659
guyon <oliv.guyon@gmail.com>
2018-06-24 14:43:04 -1000

    max privil for cset

--- src/COREMOD_tools/COREMOD_tools.c
+++ src/COREMOD_tools/COREMOD_tools.c
@@ -232,12 +232,22 @@ int COREMOD_TOOLS_mvProcCPUset(const char *csetname)
 {
     int pid;
     char command[200];
+	int r;
 
     pid = getpid();
+    
+    #ifndef __MACH__
+    
+    r = seteuid(data.euid); //This goes up to maximum privileges
+
     sprintf(command, "sudo -n cset proc -m -p %d -t %s\n", pid, csetname);
+	printf("Executing command: %s\n", command);
 
     if(system(command) != 0)
         printERROR(__FILE__,__func__,__LINE__, "system() returns non-zero value");
+    
+    r = seteuid(data.ruid);//Go back to normal privileges
+	#endif
 
     return(0);
 }
