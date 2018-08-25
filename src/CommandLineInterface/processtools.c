24e402894d7dfa617a0ed31d2bcea7b8328b2b59
guyon <oliv.guyon@gmail.com>
2018-08-24 19:49:05 -1000

    processinfo list

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -34,6 +34,8 @@
 #include <unistd.h>    // getpid()
 #include <sys/types.h>
 
+#include <sys/stat.h>
+
 #include <CommandLineInterface/CLIcore.h>
 
 
@@ -56,7 +58,7 @@
 
 
 
-
+static PROCESSINFOLIST *pinfolist;
 
 
 
@@ -72,6 +74,91 @@
 /*                                    FUNCTIONS SOURCE CODE                                        */
 /* =============================================================================================== */
 /* =============================================================================================== */
+
+
+// 
+// if list does not exist, create it and return index = 0
+// if list exists, return first available index
+//
+
+long processinfo_shm_list_create()
+{
+    char  SM_fname[200];
+	long pindex = 0;
+
+    sprintf(SM_fname, "%s/processinfo.list.shm", SHAREDMEMDIR);
+
+
+    /*
+    * Check if a file exist using stat() function.
+    * return 1 if the file exist otherwise return 0.
+    */
+    struct stat buffer;
+    int exists = stat(SM_fname, &buffer);
+
+    if(exists == 0)
+    {
+		printf("CREATING PROCESSINFO LIST\n");
+		
+        size_t sharedsize = 0; // shared memory size in bytes
+        int SM_fd; // shared memory file descriptor
+
+        sharedsize = sizeof(PROCESSINFOLIST);
+
+        SM_fd = open(SM_fname, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+        if (SM_fd == -1) {
+            perror("Error opening file for writing");
+            exit(0);
+        }
+
+        int result;
+        result = lseek(SM_fd, sharedsize-1, SEEK_SET);
+        if (result == -1) {
+            close(SM_fd);
+            fprintf(stderr, "Error calling lseek() to 'stretch' the file");
+            exit(0);
+        }
+
+        result = write(SM_fd, "", 1);
+        if (result != 1) {
+            close(SM_fd);
+            perror("Error writing last byte of the file");
+            exit(0);
+        }
+
+        pinfolist = (PROCESSINFOLIST*) mmap(0, sharedsize, PROT_READ | PROT_WRITE, MAP_SHARED, SM_fd, 0);
+        if (pinfolist == MAP_FAILED) {
+            close(SM_fd);
+            perror("Error mmapping the file");
+            exit(0);
+        }
+        pindex = 0;
+    }
+    else
+    {
+		int SM_fd;
+		struct stat file_stat;
+		
+		SM_fd = open(SM_fname, O_RDWR);
+		fstat(SM_fd, &file_stat);
+        printf("File %s size: %zd\n", SM_fname, file_stat.st_size);
+
+        pinfolist = (PROCESSINFOLIST*) mmap(0, file_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, SM_fd, 0);
+        if (pinfolist == MAP_FAILED) {
+            close(SM_fd);
+            perror("Error mmapping the file");
+            exit(0);
+        }
+        
+        while(pinfolist->active[pindex] == 1)
+			pindex ++;
+	}
+
+	printf("pindex = %ld\n", pindex);
+	
+    return pindex;
+}
+
 
 
 
@@ -93,8 +180,11 @@ PROCESSINFO* processinfo_shm_create(char *pname)
     
     PID = getpid();
 
+	long pindex;
+    pindex = processinfo_shm_list_create();
     
-    sprintf(SM_fname, "%s/%s.%d.proc.shm", SHAREDMEMDIR, pname, (int) PID);    
+    
+    sprintf(SM_fname, "%s/proc.%06d.shm", SHAREDMEMDIR, (int) PID);    
     SM_fd = open(SM_fname, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
     if (SM_fd == -1) {
         perror("Error opening file for writing");
