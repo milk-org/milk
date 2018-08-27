35af471a8abf76d13fcdb219d445f52d24ffd163
guyon <oliv.guyon@gmail.com>
2018-08-26 20:02:48 -1000

    added error case

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -821,10 +821,10 @@ int_fast8_t processinfo_CTRLscreen()
 
                     loopcntarray[pindex] = pinfoarray[pindex]->loopcnt;
 
-					if(processinfo->loopstat == 4) // ERROR
+					if(pinfoarray[pindex]->loopstat == 4) // ERROR
 						attron(COLOR_PAIR(2));
                     printw("  %40s", pinfoarray[pindex]->statusmsg);
-                    if(processinfo->loopstat == 4) // ERROR
+                    if(pinfoarray[pindex]->loopstat == 4) // ERROR
 						attroff(COLOR_PAIR(2));
                 }
                 printw("\n");
