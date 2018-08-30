ad87ccd5522664d4c71bd5d6fe2f2bc884bb53c5
guyon <oliv.guyon@gmail.com>
2018-08-29 20:55:30 -1000

    debug for long list

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -885,7 +885,7 @@ int_fast8_t processinfo_CTRLscreen()
                 pindex = pindexActive[index];
                 indexarray[index] = pindex;
                 // minus sign for most recent first
-                timearray[index] = -1.0*pinfoarray[pindex]->createtime.tv_sec - 1.0e-9*pinfoarray[pindex]->createtime.tv_nsec;
+               // timearray[index] = -1.0*pinfoarray[pindex]->createtime.tv_sec - 1.0e-9*pinfoarray[pindex]->createtime.tv_nsec;
             }
 
        //     quick_sort2l_double(timearray, indexarray, NBpindexActive);
