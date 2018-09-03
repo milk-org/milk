0f530c225f854949956bd3d4367fd75e03b530b6
guyon <oliv.guyon@gmail.com>
2018-09-02 14:17:16 -1000

    color tweak

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -747,8 +747,13 @@ static int initncurses()
     curs_set(0);
     noecho();			/* Don't echo() while we do getch */
 
+    init_color(COLOR_GREEN, 900, 1000, 900);
+	init_color(COLOR_YELLOW, 1000, 1000, 900);
+
     start_color();
     
+
+
 	//  color background
     init_pair(1, COLOR_BLACK, COLOR_WHITE);
     init_pair(2, COLOR_BLACK, COLOR_GREEN);
