a5aa79e7e8df9f2f6619683d09d758e619e8ae95
Olivier Guyon <oliv.guyon@gmail.com>
2018-10-18 18:12:59 -1000

    display mode 3

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -2802,11 +2802,11 @@ int_fast8_t processinfo_CTRLscreen()
 								quick_sort_long(dtexec_array, PROCESSINFO_NBtimer-1);
 								
 								
-								printw("[ITER-lim %d/%8ld/%4ld]  ", pinfoarray[pindex]->dtiter_limit_enable, pinfoarray[pindex]->dtiter_limit_value, pinfoarray[pindex]->dtiter_limit_value);
-								printw("[EXEC-lim %d/%8ld/%4ld] ", pinfoarray[pindex]->dtexec_limit_enable, pinfoarray[pindex]->dtexec_limit_value, pinfoarray[pindex]->dtexec_limit_value);
+								printw("| %d/%8ld/%4ld ", pinfoarray[pindex]->dtiter_limit_enable, pinfoarray[pindex]->dtiter_limit_value, pinfoarray[pindex]->dtiter_limit_value);
+								printw("| %d/%8ld/%4ld |", pinfoarray[pindex]->dtexec_limit_enable, pinfoarray[pindex]->dtexec_limit_value, pinfoarray[pindex]->dtexec_limit_value);
 								
-								printw(" dtiter %8.3fus (%8.3f - %8.3f) ", 0.001*dtiter_array[(long) (0.5*PROCESSINFO_NBtimer)], 0.001*dtiter_array[0], 0.001*dtiter_array[PROCESSINFO_NBtimer-2]);
-								printw(" dtexec %8.3fus (%8.3f - %8.3f) ", 0.001*dtexec_array[(long) (0.5*PROCESSINFO_NBtimer)], 0.001*dtexec_array[0], 0.001*dtexec_array[PROCESSINFO_NBtimer-2]);
+								printw("| %9.3fus (%9.3f - %9.3f) ", 0.001*dtiter_array[(long) (0.5*PROCESSINFO_NBtimer)], 0.001*dtiter_array[0], 0.001*dtiter_array[PROCESSINFO_NBtimer-2]);
+								printw("| %9.3fus (%9.3f - %9.3f) |", 0.001*dtexec_array[(long) (0.5*PROCESSINFO_NBtimer)], 0.001*dtexec_array[0], 0.001*dtexec_array[PROCESSINFO_NBtimer-2]);
 								
 								free(dtiter_array);
 								free(dtexec_array);
