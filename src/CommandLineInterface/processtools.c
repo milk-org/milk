b9fe47a987fad60e45b5ed7985ed0496bad89157
Olivier Guyon <oliv.guyon@gmail.com>
2018-10-18 18:28:28 -1000

    display mode 3

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -2801,7 +2801,11 @@ int_fast8_t processinfo_CTRLscreen()
 								quick_sort_long(dtiter_array, PROCESSINFO_NBtimer-1);
 								quick_sort_long(dtexec_array, PROCESSINFO_NBtimer-1);
 								
-								printw("%lc ", 0x2503);
+								char block1[] = { 0xe2, 0x94, 0x80, '\0' };
+								char block2[] = { 0xe2, 0x94, 0x81, '\0' };
+								printw("%s%s\n", block1, block2);
+    
+//								printw("%lc ", 0x2503);
 								printw("| %d/%8ld/%4ld ", pinfoarray[pindex]->dtiter_limit_enable, pinfoarray[pindex]->dtiter_limit_value, pinfoarray[pindex]->dtiter_limit_value);
 								printw("| %d/%8ld/%4ld |", pinfoarray[pindex]->dtexec_limit_enable, pinfoarray[pindex]->dtexec_limit_value, pinfoarray[pindex]->dtexec_limit_value);
 								
