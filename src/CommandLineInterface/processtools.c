d87f10f138efa448120c97498bd558b9689f0f8a
Olivier Guyon <oliv.guyon@gmail.com>
2018-10-20 23:48:49 -1000

    move help to display mode 1

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -2996,7 +2996,7 @@ int_fast8_t processinfo_CTRLscreen()
 
                     }
 
-                    if(DisplayMode == 1)
+                    if(DisplayMode == 2)
                         printw("\n");
                     if(DisplayMode == 3)
                         printw("\n");
