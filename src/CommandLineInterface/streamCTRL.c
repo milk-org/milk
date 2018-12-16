6f389bc6a9e7ea7acabe2f0cc18c05f45c97b989
Olivier Guyon <oliv.guyon@gmail.com>
2018-12-15 15:14:34 -1000

    improved streamCTRL

--- src/CommandLineInterface/streamCTRL.c
+++ src/CommandLineInterface/streamCTRL.c
@@ -60,7 +60,8 @@
 /* =============================================================================================== */
 /* =============================================================================================== */
 
-
+#define streamNBID_MAX 10000
+#define streamOpenNBpid_MAX 50
 
 
 /* =============================================================================================== */
@@ -149,7 +150,24 @@ static int initncurses()
 
 
 
-
+static const char* get_process_name_by_pid(const int pid)
+{
+    char* name = (char*)calloc(1024,sizeof(char));
+    if(name){
+        sprintf(name, "/proc/%d/cmdline",pid);
+        FILE* f = fopen(name,"r");
+        if(f){
+            size_t size;
+            size = fread(name, sizeof(char), 1024, f);
+            if(size>0){
+                if('\n'==name[size-1])
+                    name[size-1]='\0';
+            }
+            fclose(f);
+        }
+    }
+    return name;
+}
 
 
 
@@ -180,10 +198,10 @@ int_fast8_t streamCTRL_CTRLscreen()
 
     float frequ = 30.0; // Hz
     char  monstring[200];
-	
-	long IDmax = 10000;
-	long cnt0array[1000];
-	long IDarray[200];
+
+    long IDmax = 10000;
+    long cnt0array[1000];
+    long IDarray[streamNBID_MAX];
 
     setlocale(LC_ALL, "");
 
@@ -204,6 +222,9 @@ int_fast8_t streamCTRL_CTRLscreen()
     // display modes:
     // 1: overview
 
+    int fuserUpdate = 1;
+    pid_t streamOpenPIDarray[streamNBID_MAX][streamOpenNBpid_MAX];
+    int streamOpenPIDarray_cnt[streamNBID_MAX];
 
     clear();
 
@@ -261,9 +282,14 @@ int_fast8_t streamCTRL_CTRLscreen()
             DisplayMode = 4;
             break;
 
+        case KEY_F(5): // read PIDs
+            fuserUpdate = 1;
+            DisplayMode = 5;
+            break;
+
 
         case 'R': // remove stream
-            ImageStreamIO_destroyIm( &data.image[IDarray[sindex]] );
+            ImageStreamIO_destroyIm( &data.image[IDarray[sindexSelected]]);
             break;
 
         }
@@ -292,11 +318,22 @@ int_fast8_t streamCTRL_CTRLscreen()
             attron(attrval);
             printw("    F2");
             attroff(attrval);
-            printw("   Process control screen\n");
+            printw("   Display semaphore values\n");
 
+            attron(attrval);
+            printw("    F3");
+            attroff(attrval);
+            printw("   Display semaphore write PIDs\n");
 
+            attron(attrval);
+            printw("    F4");
+            attroff(attrval);
+            printw("   Display semaphore read PIDs\n");
 
-
+            attron(attrval);
+            printw("    F5");
+            attroff(attrval);
+            printw("   stream open by processes ...\n");
 
             attron(attrval);
             printw("SPACE");
@@ -309,6 +346,66 @@ int_fast8_t streamCTRL_CTRLscreen()
         }
         else
         {
+
+			if(DisplayMode==1)
+			{
+				attron(A_REVERSE);
+				printw("[h] Help");
+				attroff(A_REVERSE);
+			}
+			else
+				printw("[h] Help");
+			printw("   ");
+			
+			if(DisplayMode==2)
+			{
+				attron(A_REVERSE);
+				printw("[F2] sem values");
+				attroff(A_REVERSE);
+			}
+			else
+				printw("[F2] sem values");
+			printw("   ");	
+
+			if(DisplayMode==3)
+			{
+				attron(A_REVERSE);
+				printw("[F3] write PIDs");
+				attroff(A_REVERSE);
+			}
+			else
+				printw("[F3] write PIDs");
+			printw("   ");	
+
+			if(DisplayMode==4)
+			{
+				attron(A_REVERSE);
+				printw("[F4] read PIDs");
+				attroff(A_REVERSE);
+			}
+			else
+				printw("[F4] read PIDs");
+			printw("   ");	
+
+			if(DisplayMode==5)
+			{
+				attron(A_REVERSE);
+				printw("[F5] processes access");
+				attroff(A_REVERSE);
+			}
+			else
+				printw("[F5] processes access");
+			printw("   ");	
+
+
+			printw("\n\n");
+
+
+
+
+
+
+
 
             DIR *d;
             struct dirent *dir;
@@ -340,100 +437,193 @@ int_fast8_t streamCTRL_CTRLscreen()
                         printw("%03ld %4ld  %-36s ", sindex, ID, sname);
                         IDarray[sindex] = ID;
 
-
-                        int atype = data.image[ID].md[0].atype;
-                        char str[200];
-                        char str1[200];
-                        int j;
-
-
-                        if(atype==_DATATYPE_UINT8)
-                            printw(" UI8");
-                        if(atype==_DATATYPE_INT8)
-                            printw("  I8");
-
-                        if(atype==_DATATYPE_UINT16)
-                            printw("UI16");
-                        if(atype==_DATATYPE_INT16)
-                            printw(" I16");
-
-                        if(atype==_DATATYPE_UINT32)
-                            printw("UI32");
-                        if(atype==_DATATYPE_INT32)
-                            printw(" I32");
-
-                        if(atype==_DATATYPE_UINT64)
-                            printw("UI64");
-                        if(atype==_DATATYPE_INT64)
-                            printw(" I64");
-
-                        if(atype==_DATATYPE_FLOAT)
-                            printw(" FLT");
-
-                        if(atype==_DATATYPE_DOUBLE)
-                            printw(" DBL");
-
-                        if(atype==_DATATYPE_COMPLEX_FLOAT)
-                            printw("CFLT");
-
-                        if(atype==_DATATYPE_COMPLEX_DOUBLE)
-                            printw("CDBL");
-
-                        sprintf(str, " [%3ld", (long) data.image[ID].md[0].size[0]);
-
-                        for(j=1; j<data.image[ID].md[0].naxis; j++)
+                        if(DisplayMode < 5)
                         {
-                            sprintf(str1, "%sx%3ld", str, (long) data.image[ID].md[0].size[j]);
+                            int atype = data.image[ID].md[0].atype;
+                            char str[200];
+                            char str1[200];
+                            int j;
+
+
+                            if(atype==_DATATYPE_UINT8)
+                                printw(" UI8");
+                            if(atype==_DATATYPE_INT8)
+                                printw("  I8");
+
+                            if(atype==_DATATYPE_UINT16)
+                                printw("UI16");
+                            if(atype==_DATATYPE_INT16)
+                                printw(" I16");
+
+                            if(atype==_DATATYPE_UINT32)
+                                printw("UI32");
+                            if(atype==_DATATYPE_INT32)
+                                printw(" I32");
+
+                            if(atype==_DATATYPE_UINT64)
+                                printw("UI64");
+                            if(atype==_DATATYPE_INT64)
+                                printw(" I64");
+
+                            if(atype==_DATATYPE_FLOAT)
+                                printw(" FLT");
+
+                            if(atype==_DATATYPE_DOUBLE)
+                                printw(" DBL");
+
+                            if(atype==_DATATYPE_COMPLEX_FLOAT)
+                                printw("CFLT");
+
+                            if(atype==_DATATYPE_COMPLEX_DOUBLE)
+                                printw("CDBL");
+
+                            sprintf(str, " [%3ld", (long) data.image[ID].md[0].size[0]);
+
+                            for(j=1; j<data.image[ID].md[0].naxis; j++)
+                            {
+                                sprintf(str1, "%sx%3ld", str, (long) data.image[ID].md[0].size[j]);
+                                strcpy(str, str1);
+                            }
+                            sprintf(str1, "%s]", str);
                             strcpy(str, str1);
+
+                            printw("%-20s ", str);
+
+
+                            // counter and semaphores
+                            if(data.image[ID].md[0].cnt0 == cnt0array[ID]) // has not changed
+                            {
+                                printw(" %8ld", data.image[ID].md[0].cnt0);
+                            }
+                            else
+                            {
+                                attron(COLOR_PAIR(2));
+                                printw(" %8ld", data.image[ID].md[0].cnt0);
+                                attroff(COLOR_PAIR(2));
+                            }
+                            cnt0array[ID] = data.image[ID].md[0].cnt0;
+
                         }
-                        sprintf(str1, "%s]", str);
-                        strcpy(str, str1);
-
-                        printw("%-20s ", str);
 
 
-                        // counter and semaphores
-                        if(data.image[ID].md[0].cnt0 == cnt0array[ID]) // has not changed
-                        {
-							printw(" %8ld", data.image[ID].md[0].cnt0);
-						}
-						else
-						{
-							attron(COLOR_PAIR(2));
-							printw(" %6ld", data.image[ID].md[0].cnt0);
-							attroff(COLOR_PAIR(2));
-						}
-						cnt0array[ID] = data.image[ID].md[0].cnt0;
-                        
-                        
-                        
-                        printw(" [%3ld sems ", data.image[ID].md[0].sem);
-                        int s;
-                        
+
                         if(DisplayMode == 2) // sem vals
                         {
-							for(s=0; s<data.image[ID].md[0].sem; s++)
-							{
-								int semval;
-								sem_getvalue(data.image[ID].semptr[s], &semval);
-								printw(" %6d ", semval);
-							}
-						}
-						if(DisplayMode == 3) // sem write PIDs
+                            printw(" [%3ld sems ", data.image[ID].md[0].sem);
+                            int s;
+                            for(s=0; s<data.image[ID].md[0].sem; s++)
+                            {
+                                int semval;
+                                sem_getvalue(data.image[ID].semptr[s], &semval);
+                                printw(" %7d", semval);
+                            }
+                            printw(" ]");
+                        }
+                        if(DisplayMode == 3) // sem write PIDs
                         {
-							for(s=0; s<data.image[ID].md[0].sem; s++)
-							{
-								printw(" %6d ", data.image[ID].semWritePID);
-							}
-						}
-						if(DisplayMode == 4) // sem read PIDs
+                            printw(" [%3ld sems ", data.image[ID].md[0].sem);
+                            int s;
+                            for(s=0; s<data.image[ID].md[0].sem; s++)
+                            {
+                                pid_t pid = data.image[ID].semWritePID[s];
+                                if(getpgid(pid) >= 0)
+                                {
+                                    attron(COLOR_PAIR(2));
+                                    printw("%7d", pid);
+                                    attroff(COLOR_PAIR(2));
+                                }
+                                else
+                                {
+                                    if(pid>0)
+                                    {
+                                        attron(COLOR_PAIR(4));
+                                        printw("%7d", pid);
+                                        attroff(COLOR_PAIR(4));
+                                    }
+                                    else
+                                        printw("%7d", pid);
+                                }
+                                printw(" ]");
+                            }
+                        }
+                        if(DisplayMode == 4) // sem read PIDs
                         {
-							for(s=0; s<data.image[ID].md[0].sem; s++)
-							{
-								printw(" %6d ", data.image[ID].semReadPID);
-							}
-						}
-                        printw("]");
+                            printw(" [%3ld sems ", data.image[ID].md[0].sem);
+                            int s;
+                            for(s=0; s<data.image[ID].md[0].sem; s++)
+                            {
+                                pid_t pid = data.image[ID].semReadPID[s];
+                                if(getpgid(pid) >= 0)
+                                {
+                                    attron(COLOR_PAIR(2));
+                                    printw("%7d", pid);
+                                    attroff(COLOR_PAIR(2));
+                                }
+                                else
+                                {
+                                    if(pid>0)
+                                    {
+                                        attron(COLOR_PAIR(4));
+                                        printw("%7d", pid);
+                                        attroff(COLOR_PAIR(4));
+                                    }
+                                    else
+                                        printw("%7d", pid);
+                                }
+                                printw(" ]");
+                            }
+                        }
+
+                        if(DisplayMode == 5) // open by processes...
+                        {
+                            if(fuserUpdate==1)
+                            {
+                                FILE *fp;
+                                char fuseroutline[1035];
+                                char command[2000];
+
+                                /* Open the command for reading. */
+                                sprintf(command, "/bin/fuser /tmp/%s.im.shm 2>/dev/null", sname);
+                                fp = popen(command, "r");
+                                if (fp == NULL) {
+                                    printf("Failed to run command\n" );
+                                    exit(1);
+                                }
+                                /* Read the output a line at a time - output it. */
+                                if (fgets(fuseroutline, sizeof(fuseroutline)-1, fp) != NULL) {
+                                    //printw("  OPEN BY: %-30s", fuseroutline);
+                                }
+                                pclose(fp);
+
+
+                                char * pch;
+                                int NBpid = 0;
+
+                                pch = strtok (fuseroutline," ");
+
+                                while (pch != NULL) {
+                                    if(NBpid<streamOpenNBpid_MAX) {
+                                        streamOpenPIDarray[sindex][NBpid] = atoi(pch);
+                                        if(getpgid(pid) >= 0)
+                                            NBpid++;
+                                    }
+                                    pch = strtok (NULL, " ");
+                                }
+                                streamOpenPIDarray_cnt[sindex] = NBpid;
+
+
+                                fuserUpdate = 0;
+                            }
+
+                            printw("  OPENED BY: ");
+                            int pidIndex;
+                            for(pidIndex=0; pidIndex<streamOpenPIDarray_cnt[sindex] ; pidIndex++)
+                            {
+                                pid_t pid = streamOpenPIDarray[sindex][pidIndex];
+                                if(getpgid(pid) >= 0)
+                                    printw(" %s(%d)", get_process_name_by_pid(pid), (int) pid);
+                            }
+                        }
 
 
                         printw("\n");
