0cad2b6d1a5fb4890462edb4d9c481e26ebee86e
Olivier Guyon <oliv.guyon@gmail.com>
2018-12-21 22:34:29 -1000

    filesyst mode

--- src/CommandLineInterface/streamCTRL.c
+++ src/CommandLineInterface/streamCTRL.c
@@ -1006,7 +1006,7 @@ int_fast8_t streamCTRL_CTRLscreen()
                                 fp = fopen(plistfname, "r");
                                 if (fp == NULL) {
                                     streamOpenPIDarray_status[ID] = 2; // failed
-                                    printw(" A");
+                                    printw(" A  %s", plistfname);
                                 }
                                 else
                                 {
