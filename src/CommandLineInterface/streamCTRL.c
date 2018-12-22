12053716141d48a7fcc75378aa64fc00b0e3ae76
Olivier Guyon <oliv.guyon@gmail.com>
2018-12-21 23:53:41 -1000

    filesyst mode

--- src/CommandLineInterface/streamCTRL.c
+++ src/CommandLineInterface/streamCTRL.c
@@ -1090,10 +1090,10 @@ int_fast8_t streamCTRL_CTRLscreen()
                                     sprintf(lstring, "%s %6d:%-*.*s", lstring, (int) pid, PIDnameStringLen, PIDnameStringLen, PIDname_array[pid]);
                                     streamOpenPIDarray_cnt1[ID]++;
                                 }
-                               // printw("%s", lstring);
+                               printw("%s", lstring);
 
-								const chtype * lstring1 = "This is a test";
-                                addchstr(lstring1);
+								//const chtype * lstring1 = "This is a test";
+                                //addchstr(lstring1);
                             }
                             break;
 
