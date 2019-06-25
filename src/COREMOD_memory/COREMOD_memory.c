dad1b68fa4064065d233aca0856482dac3d1cc64
Olivier Guyon <oliv.guyon@gmail.com>
2019-06-25 04:19:19 -1000

    sem changed to 6

--- src/COREMOD_memory/COREMOD_memory.c
+++ src/COREMOD_memory/COREMOD_memory.c
@@ -6818,7 +6818,7 @@ long COREMOD_MEMORY_image_NETWORKtransmit(
     char *buff; // transmit buffer
 
 
-    int semtrig = 8; // TODO - scan for available sem
+    int semtrig = 6; // TODO - scan for available sem
     // IMPORTANT: do not use semtrig 0 
     int UseSem = 1;
 
