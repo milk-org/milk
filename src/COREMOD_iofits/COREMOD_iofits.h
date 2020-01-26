0f14292faad7b506165a23f478a39f3064a34768
Olivier Guyon <oliv.guyon@gmail.com>
2020-01-26 13:55:22 -1000

    minor cleanup

--- src/COREMOD_iofits/COREMOD_iofits.h
+++ src/COREMOD_iofits/COREMOD_iofits.h
@@ -12,37 +12,106 @@ int file_exists(const char * restrict file_name);
 
 int is_fits_file(const char * restrict file_name);
 
-int read_keyword(const char * restrict file_name, const char * restrict KEYWORD, char * restrict content);
+int read_keyword(
+    const char* restrict file_name,
+    const char* restrict KEYWORD,
+    char* restrict content
+);
 
-int read_keyword_alone(const char * restrict file_name, const char * restrict KEYWORD);
+errno_t read_keyword_alone(
+    const char* restrict file_name,
+    const char* restrict KEYWORD
+);
+
 
 int data_type_code(int bitpix);
 
-long load_fits(const char * restrict file_name, const char * restrict ID_name, int errcode); 
 
-int save_db_fits(const char * restrict ID_name, const char * restrict file_name);
-
-int save_fl_fits(const char * restrict ID_name, const char * restrict file_name);
-
-int save_sh16_fits(const char * restrict ID_name, const char * restrict file_name);
-int save_ush16_fits(const char * restrict ID_name, const char * restrict file_name);
-int save_int32_fits(const char * restrict ID_name, const char * restrict file_name);
-int save_uint32_fits(const char * restrict ID_name, const char * restrict file_name);
-int save_long64_fits(const char * restrict ID_name, const char * restrict file_name);
+imageID load_fits(
+    const char* restrict file_name,
+    const char* restrict ID_name,
+    int         errcode
+);
 
 
-int save_fits(const char * restrict ID_name, const char * restrict file_name);
-int save_fits_atomic(const char * restrict ID_name, const char * restrict file_name);
-
-int saveall_fits(const char * restrict savedirname);
-
-int break_cube(const char * restrict ID_name);
-
-int images_to_cube(const char * restrict img_name, long nbframes, const char * restrict cube_name);
+errno_t save_db_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
 
 
+errno_t save_fl_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
 
-long COREMOD_IOFITS_LoadMemStream(const char *sname, uint64_t *streamflag, uint32_t *imLOC);
+
+errno_t save_sh16_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t save_ush16_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t save_int32_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t save_uint32_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t save_int64_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+
+
+errno_t save_fits(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t save_fits_atomic(
+    const char* restrict ID_name,
+    const char* restrict file_name
+);
+
+
+errno_t saveall_fits(
+    const char* restrict savedirname
+);
+
+
+imageID break_cube(
+    const char* restrict ID_name
+);
+
+errno_t images_to_cube(
+    const char* restrict img_name,
+    long                 nbframes,
+    const char* restrict cube_name
+);
+
+
+imageID COREMOD_IOFITS_LoadMemStream(
+    const char *sname,
+    uint64_t   *streamflag,
+    uint32_t   *imLOC
+);
+
 
 
 #endif
