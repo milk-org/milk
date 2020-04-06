9ab89ca0abfe11d9883f43669d3ca2672fcd9596
oguyon <oliv.guyon@gmail.com>
2020-04-06 08:54:17 -1000

    cleaner module init

--- src/COREMOD_iofits/COREMOD_iofits.h
+++ src/COREMOD_iofits/COREMOD_iofits.h
@@ -3,23 +3,23 @@
 
 
 
-void __attribute__ ((constructor)) libinit_COREMOD_iofits();
+void __attribute__((constructor)) libinit_COREMOD_iofits();
 
 
 
-int file_exists(const char * restrict file_name);
+int file_exists(const char *restrict file_name);
 
-int is_fits_file(const char * restrict file_name);
+int is_fits_file(const char *restrict file_name);
 
 int read_keyword(
-    const char* restrict file_name,
-    const char* restrict KEYWORD,
-    char* restrict content
+    const char *restrict file_name,
+    const char *restrict KEYWORD,
+    char *restrict content
 );
 
 errno_t read_keyword_alone(
-    const char* restrict file_name,
-    const char* restrict KEYWORD
+    const char *restrict file_name,
+    const char *restrict KEYWORD
 );
 
 
@@ -27,81 +27,81 @@ int data_type_code(int bitpix);
 
 
 imageID load_fits(
-    const char* restrict file_name,
-    const char* restrict ID_name,
+    const char *restrict file_name,
+    const char *restrict ID_name,
     int         errcode
 );
 
 
 errno_t save_db_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_fl_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_sh16_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_ush16_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_int32_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_uint32_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_int64_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 
 
 errno_t save_fits(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t save_fits_atomic(
-    const char* restrict ID_name,
-    const char* restrict file_name
+    const char *restrict ID_name,
+    const char *restrict file_name
 );
 
 
 errno_t saveall_fits(
-    const char* restrict savedirname
+    const char *restrict savedirname
 );
 
 
 imageID break_cube(
-    const char* restrict ID_name
+    const char *restrict ID_name
 );
 
 errno_t images_to_cube(
-    const char* restrict img_name,
+    const char *restrict img_name,
     long                 nbframes,
-    const char* restrict cube_name
+    const char *restrict cube_name
 );
 
 
