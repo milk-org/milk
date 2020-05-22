a52a7878f8de0ca32e52675c078a5f3bddbaa5df
oguyon <oliv.guyon@gmail.com>
2020-05-21 20:03:49 -1000

    handle old readline

--- src/CommandLineInterface/CLIcore.c
+++ src/CommandLineInterface/CLIcore.c
@@ -2063,7 +2063,7 @@ errno_t runCLI(
 	printf("READLINE VERSION : %x\n", RL_READLINE_VERSION);
 	#if ( RL_READLINE_VERSION > 0x620 )
 	printf("WILL CLEAR\n");
-	//rl_clear_history();
+	rl_clear_history();
 	#endif
 
     rl_callback_handler_remove();
