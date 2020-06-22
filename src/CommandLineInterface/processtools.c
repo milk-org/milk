2abee29ef865e69ef4113e0eb62f904285c7571f
oguyon <oliv.guyon@gmail.com>
2020-06-21 21:30:18 -1000

    display upstream and downstream

--- src/CommandLineInterface/processtools.c
+++ src/CommandLineInterface/processtools.c
@@ -1340,6 +1340,7 @@ errno_t processinfo_update_output_stream(
     data.image[outstreamID].streamproctrace[0].ts_procstart     = processinfo->texecstart[processinfo->timerindex];
     data.image[outstreamID].streamproctrace[0].ts_streamupdate  = ts;
     data.image[outstreamID].streamproctrace[0].trigsemindex     = processinfo->triggersem;
+    data.image[outstreamID].streamproctrace[0].triggerstatus    = processinfo->triggerstatus;
     data.image[outstreamID].streamproctrace[0].cnt0             = data.image[IDin].md[0].cnt0;
 
 
@@ -2610,7 +2611,7 @@ void *processinfo_scan(
 
 
 
-        if(pinfop->DisplayMode == 3) { // only compute of displayed processes
+        if(pinfop->DisplayMode == PROCCTRL_DISPLAYMODE_RESOURCES) { // only compute of displayed processes
             DEBUG_TRACEPOINT(" ");
             pinfop->scandebugline = __LINE__;
             GetCPUloads(pinfop);
@@ -3151,6 +3152,8 @@ errno_t processinfo_CTRLscreen()
             break;
 
 
+
+
         case KEY_UP:
             pindexActiveSelected --;
             if(pindexActiveSelected<0)
@@ -3170,6 +3173,17 @@ errno_t processinfo_CTRLscreen()
             else
                 pindexSelected = procinfoproc.sorted_pindex_time[pindexActiveSelected];
             break;
+            
+            
+        case KEY_RIGHT:
+			procinfoproc.DisplayDetailedMode = 1;
+			break;
+		
+		
+		case KEY_LEFT:
+			procinfoproc.DisplayDetailedMode = 0;
+			break;
+
 
         case KEY_PPAGE:
             pindexActiveSelected -= 10;
@@ -4198,6 +4212,9 @@ errno_t processinfo_CTRLscreen()
                 // ===========================================================================
                 pstrlen_total_max = 0;
 				pindexSelectedOK = 0;
+				
+				
+				
                 for(dispindex=0; dispindex < dispindexMax; dispindex++)
                 {
                     if(TimeSorted == 0)
