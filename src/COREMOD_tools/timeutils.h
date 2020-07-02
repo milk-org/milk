95fcf7c4a1babef01da856944fcd713283d5d52d
oguyon <oliv.guyon@gmail.com>
2020-07-02 12:34:32 -1000

    sync

--- src/COREMOD_tools/timeutils.h
+++ src/COREMOD_tools/timeutils.h
@@ -2,6 +2,12 @@
  * @file timeutils.h
  */
 
+errno_t mkUTtimestring_nanosec(char *timestring);
+errno_t mkUTtimestring_microsec(char *timestring);
+errno_t mkUTtimestring_millisec(char *timestring);
+errno_t mkUTtimestring_sec(char *timestring);
+
+
 struct timespec timespec_diff(
     struct timespec start,
     struct timespec end
