ac03097e7653267ab394760d00e140028798150e
oguyon <oliv.guyon@gmail.com>
2020-07-02 12:34:32 -1000

    sync

--- src/COREMOD_tools/timeutils.c
+++ src/COREMOD_tools/timeutils.c
@@ -3,6 +3,95 @@
  */
 
 #include <time.h>
+#include "CommandLineInterface/CLIcore.h"
+
+
+errno_t mkUTtimestring_nanosec(char *timestring)
+{
+    struct timespec tnow;
+	struct tm *uttime;
+    time_t tvsec0;
+    
+    
+    clock_gettime(CLOCK_REALTIME, &tnow);
+    tvsec0 = tnow.tv_sec;
+    uttime = gmtime(&tvsec0);
+
+    sprintf(timestring, "%04d%02d%02dT%02d%02d%02d.%09ld",
+            1900 + uttime->tm_year, 1 + uttime->tm_mon, uttime->tm_mday, uttime->tm_hour,
+            uttime->tm_min,  uttime->tm_sec, tnow.tv_nsec);
+
+
+    return RETURN_SUCCESS;
+}
+
+
+errno_t mkUTtimestring_microsec(char *timestring)
+{
+    struct timespec tnow;
+	struct tm *uttime;
+    time_t tvsec0;
+    
+    
+    clock_gettime(CLOCK_REALTIME, &tnow);
+    tvsec0 = tnow.tv_sec;
+    uttime = gmtime(&tvsec0);
+
+    sprintf(timestring, "%04d%02d%02dT%02d%02d%02d.%06ld",
+            1900 + uttime->tm_year, 1 + uttime->tm_mon, uttime->tm_mday, uttime->tm_hour,
+            uttime->tm_min,  uttime->tm_sec, (long) (tnow.tv_nsec/1000));
+
+
+    return RETURN_SUCCESS;
+}
+
+
+errno_t mkUTtimestring_millisec(char *timestring)
+{
+    struct timespec tnow;
+	struct tm *uttime;
+    time_t tvsec0;
+    
+    
+    clock_gettime(CLOCK_REALTIME, &tnow);
+    tvsec0 = tnow.tv_sec;
+    uttime = gmtime(&tvsec0);
+
+    sprintf(timestring, "%04d%02d%02dT%02d%02d%02d.%03ld",
+            1900 + uttime->tm_year, 1 + uttime->tm_mon, uttime->tm_mday, uttime->tm_hour,
+            uttime->tm_min,  uttime->tm_sec, (long) (tnow.tv_nsec/1000000));
+
+
+    return RETURN_SUCCESS;
+}
+
+
+
+
+
+
+errno_t mkUTtimestring_sec(char *timestring)
+{
+    struct timespec tnow;
+	struct tm *uttime;
+    time_t tvsec0;
+    
+    
+    clock_gettime(CLOCK_REALTIME, &tnow);
+    tvsec0 = tnow.tv_sec;
+    uttime = gmtime(&tvsec0);
+
+    sprintf(timestring, "%04d%02d%02dT%02d%02d%02d",
+            1900 + uttime->tm_year, 1 + uttime->tm_mon, uttime->tm_mday, uttime->tm_hour,
+            uttime->tm_min,  uttime->tm_sec);
+
+
+    return RETURN_SUCCESS;
+}
+
+
+
+
 
 
 struct timespec timespec_diff(struct timespec start, struct timespec end)
