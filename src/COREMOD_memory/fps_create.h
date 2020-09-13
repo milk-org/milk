1e04a3264172233c45ae471c04f7d0891ca5f450
oguyon <oliv.guyon@gmail.com>
2020-09-13 11:19:44 -1000

    add ifndef in include files

--- src/COREMOD_memory/fps_create.h
+++ src/COREMOD_memory/fps_create.h
@@ -2,6 +2,8 @@
  * @file    fps_create.h
  */
 
+#ifndef _FPS_CREATE_H
+#define FPS_CREATE_H
 
 
 errno_t fps_create_addCLIcmd();
@@ -10,3 +12,5 @@ errno_t function_parameter_struct_create(
     int NBparamMAX,
     const char *name
 );
+
+#endif
