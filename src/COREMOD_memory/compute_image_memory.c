277a0cad0beaecba93e4c14d131dd1326de947de
oguyon <oliv.guyon@gmail.com>
2020-11-23 17:57:39 -1000

    minor syntax tweaks

--- src/COREMOD_memory/compute_image_memory.c
+++ src/COREMOD_memory/compute_image_memory.c
@@ -12,9 +12,6 @@ uint64_t compute_image_memory()
 {
     uint64_t totalmem = 0;
 
-//	printf("Computing num images\n");
-//	fflush(stdout);
-
     for(imageID i = 0; i < data.NB_MAX_IMAGE; i++)
     {
         //printf("%5ld / %5ld  %d\n", i, data.NB_MAX_IMAGE, data.image[i].used);
