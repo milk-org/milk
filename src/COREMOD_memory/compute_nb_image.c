c9e01f19ca919bbe35c6a59ffbea8343a52dfb2e
oguyon <oliv.guyon@gmail.com>
2020-11-23 17:57:39 -1000

    minor syntax tweaks

--- src/COREMOD_memory/compute_nb_image.c
+++ src/COREMOD_memory/compute_nb_image.c
@@ -10,6 +10,9 @@ long compute_nb_image()
 {
     long NBimage = 0;
 
+	//printf("NB_MAX_IMAGE = %d\n", data.NB_MAX_IMAGE);
+	//fflush(stdout);
+
     for(imageID i = 0; i < data.NB_MAX_IMAGE; i++)
     {
         if(data.image[i].used == 1)
