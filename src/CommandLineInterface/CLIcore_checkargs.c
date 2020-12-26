b02812a16719f0a5a48ba0d27555a804742e2d19
oguyon <oliv.guyon@gmail.com>
2020-12-25 23:31:42 -1000

    CLI argument parsing cleanup

--- src/CommandLineInterface/CLIcore_checkargs.c
+++ src/CommandLineInterface/CLIcore_checkargs.c
@@ -24,13 +24,13 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
     switch(argtype)
     {
 
-        case 1:  // should be floating point
+        case CLIARG_FLOAT:  // should be floating point
             switch(data.cmdargtoken[argnum].type)
             {
-                case 1:
+                case CLIARG_FLOAT:
                     rval = 0;
                     break;
-                case 2: // convert long to float
+                case CLIARG_LONG: // convert long to float
                     if(data.Debug > 0)
                     {
                         printf("Converting arg %d to floating point number\n", argnum);
@@ -39,7 +39,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                     data.cmdargtoken[argnum].type = 1;
                     rval = 0;
                     break;
-                case 3:
+                case CLIARG_STR_NOT_IMG:
                     IDv = variable_ID(data.cmdargtoken[argnum].val.string);
                     if(IDv == -1)
                     {
@@ -75,7 +75,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                         }
                     }
                     break;
-                case 4:
+                case CLIARG_IMG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is image (=\"%s\"), but should be floating point number\n",
@@ -83,7 +83,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                     }
                     rval = 1;
                     break;
-                case 5:
+                case CLIARG_STR:
                     if(errmsg == 1)
                     {
                         printf("arg %d is command (=\"%s\"), but should be floating point number\n",
@@ -99,10 +99,10 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
             }
             break;
 
-        case 2:  // should be integer
+        case CLIARG_LONG:  // should be integer
             switch(data.cmdargtoken[argnum].type)
             {
-                case 1:
+                case CLIARG_FLOAT:
                     if(errmsg == 1)
                     {
                         printf("converting floating point arg %d to integer\n", argnum);
@@ -112,10 +112,10 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                     data.cmdargtoken[argnum].type = 2;
                     rval = 0;
                     break;
-                case 2:
+                case CLIARG_LONG:
                     rval = 0;
                     break;
-                case 3:
+                case CLIARG_STR_NOT_IMG:
                     IDv = variable_ID(data.cmdargtoken[argnum].val.string);
                     if(IDv == -1)
                     {
@@ -151,7 +151,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                         }
                     }
                     break;
-                case 4:
+                case CLIARG_IMG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is image (=\"%s\"), but should be integer\n", argnum,
@@ -159,7 +159,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                     }
                     rval = 1;
                     break;
-                case 5:
+                case CLIARG_STR:
                     if(errmsg == 1)
                     {
                         printf("arg %d is command (=\"%s\"), but should be integer\n", argnum,
@@ -173,24 +173,24 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
         case 3:  // should be string, but not image
             switch(data.cmdargtoken[argnum].type)
             {
-                case 1:
+                case CLIARG_FLOAT:
                     if(errmsg == 1)
                     {
                         printf("arg %d is floating point, but should be string\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 2:
+                case CLIARG_LONG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is integer, but should be string\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 3:
+                case CLIARG_STR_NOT_IMG:
                     rval = 0;
                     break;
-                case 4:
+                case CLIARG_IMG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is existing image (=\"%s\"), but should be string\n", argnum,
@@ -198,7 +198,7 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
                     }
                     rval = 1;
                     break;
-                case 5:
+                case CLIARG_STR:
                     printf("arg %d is command (=\"%s\"), but should be string\n", argnum,
                            data.cmdargtoken[argnum].val.string);
                     rval = 1;
@@ -212,31 +212,31 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
         case 4:  // should be existing image
             switch(data.cmdargtoken[argnum].type)
             {
-                case 1:
+                case CLIARG_FLOAT:
                     if(errmsg == 1)
                     {
                         printf("arg %d is floating point, but should be image\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 2:
+                case CLIARG_LONG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is integer, but should be image\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 3:
+                case CLIARG_STR_NOT_IMG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is string, but should be image\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 4:
+                case CLIARG_IMG:
                     rval = 0;
                     break;
-                case 5:
+                case CLIARG_STR:
                     if(errmsg == 1)
                     {
                         printf("arg %d is command (=\"%s\"), but should be image\n", argnum,
@@ -252,27 +252,27 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg)
         case 5: // should be string (image or not)
             switch(data.cmdargtoken[argnum].type)
             {
-                case 1:
+                case CLIARG_FLOAT:
                     if(errmsg == 1)
                     {
                         printf("arg %d is floating point, but should be string or image\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 2:
+                case CLIARG_LONG:
                     if(errmsg == 1)
                     {
                         printf("arg %d is integer, but should be string or image\n", argnum);
                     }
                     rval = 1;
                     break;
-                case 3:
+                case CLIARG_STR_NOT_IMG:
                     rval = 0;
                     break;
-                case 4:
+                case CLIARG_IMG:
                     rval = 0;
                     break;
-                case 5:
+                case CLIARG_STR:
                     if(errmsg == 1)
                     {
                         printf("arg %d is command (=\"%s\"), but should be image\n", argnum,
@@ -321,6 +321,41 @@ int CLI_checkarg_noerrmsg(int argnum, int argtype)
 
     rval = CLI_checkarg0(argnum, argtype, 0);
     return rval;
+}
+
+
+
+
+
+/** @brief Check array of command line (CLI) arguments
+ *
+ */
+errno_t CLI_checkarg_array(
+    FPSCLIARG fpscliarg[],
+    int nbarg
+)
+{
+    printf("Number of CLI args : %d\n", nbarg);
+
+    int nberr = 0;
+    for(int arg = 0; arg < nbarg; arg++)
+    {
+        if(CLI_checkarg(arg + 1, fpscliarg[arg].type) != 0)
+        {
+            nberr ++;
+        }
+    }
+
+    printf("Number of arg error(s): %d\n", nberr);
+
+    if(nberr == 0)
+    {
+        return RETURN_SUCCESS;
+    }
+    else
+    {
+        return RETURN_FAILURE;
+    }
 }
 
 
--- src/CommandLineInterface/CLIcore_checkargs.h
+++ src/CommandLineInterface/CLIcore_checkargs.h
@@ -5,10 +5,43 @@
  *
  */
 
-
 #ifndef CLICORE_CHECKARGS_H
-
 #define CLICORE_CHECKARGS_H
+
+
+
+
+
+
+
+// testing argument type for command line interface
+#define CLIARG_FLOAT            1  // floating point number
+#define CLIARG_LONG             2  // integer (int or long)
+#define CLIARG_STR_NOT_IMG      3  // string, not existing image
+#define CLIARG_IMG              4  // existing image
+#define CLIARG_STR              5  // string
+
+
+typedef struct
+{
+	int type;
+	char fpstag[100];
+	char descr[100];
+	char example[100];
+} FPSCLIARG;
+
+
+
+#define CLICMD_SUCCESS          0
+#define CLICMD_INVALID_ARG      1
+#define CLICMD_ERROR            2
+
+
+
+
+
+
+
 
 
 int CLI_checkarg0(int argnum, int argtype, int errmsg);
@@ -16,6 +49,11 @@ int CLI_checkarg0(int argnum, int argtype, int errmsg);
 int CLI_checkarg(int argnum, int argtype);
 
 int CLI_checkarg_noerrmsg(int argnum, int argtype);
+
+errno_t CLI_checkarg_array(
+    FPSCLIARG fpscliarg[],
+    int nbarg
+);
 
 
 #endif
