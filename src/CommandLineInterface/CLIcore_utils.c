daa4851f935f5e07bffc09b390e0b40a6331086f
oguyon <oliv.guyon@gmail.com>
2021-01-01 23:41:00 -1000

    keep placeholder for future funcs

--- src/CommandLineInterface/CLIcore_utils.c
+++ src/CommandLineInterface/CLIcore_utils.c
@@ -2,11 +2,7 @@
  * @file    CLIcore_utils.c
  * @brief   Util functions for coding convenience and performance
  * 
+ * 
  */
 
-
-#include <string.h>
-
-#include "CommandLineInterface/CLIcore.h"
-#include "COREMOD_memory/COREMOD_memory.h"
-
+// currently empty placeholder
