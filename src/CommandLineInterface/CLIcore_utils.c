4c86ffa4d9749279734832923f212f94a4c8af17
oguyon <oliv.guyon@gmail.com>
2021-01-01 14:52:13 -1000

    move include to avoid undef data

--- src/CommandLineInterface/CLIcore_utils.c
+++ src/CommandLineInterface/CLIcore_utils.c
@@ -1,6 +1,6 @@
 /**
  * @file    CLIcore_utils.c
- * @brief   Util functions for coding convenience
+ * @brief   Util functions for coding convenience and performance
  * 
  */
 
@@ -9,38 +9,4 @@
 
 #include "CommandLineInterface/CLIcore.h"
 #include "COREMOD_memory/COREMOD_memory.h"
-
-//#include "ImageStreamIO/ImageStreamIO.h"
-//#include "CommandLineInterface/IMGID.h"
-
-
-
-inline IMGID makeIMGID(
-    const char *restrict name
-)
-{
-    IMGID img;
-
-    img.ID = -1;
-    strcpy(img.name, name);
-
-    return img;
-}
-
-inline imageID resolveIMGID(
-    IMGID *img
-)
-{
-    if(img->ID == -1)
-    {
-        img->ID = image_ID(img->name);
-        if(img->ID > -1)
-        {
-            img->im = &data.image[img->ID];
-            img->md = &data.image[img->ID].md[0];
-        }
-    }
-
-    return img->ID;
-}
 
