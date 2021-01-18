8543dc23ea1da0cdfce27bfa7aa55814a7f737dc
oguyon <oliv.guyon@gmail.com>
2021-01-17 21:17:53 -1000

    create extra directory

--- fetch_milk_dev.sh
+++ fetch_milk_dev.sh
@@ -1,3 +1,4 @@
+mkdir -p extra
 cd extra
 for mod in fft ZernikePolyn cudacomp image_basic image_filter image_format image_gen img_reduce info kdtree linARfilterPred linopt_imtools psf statistic 
 do
