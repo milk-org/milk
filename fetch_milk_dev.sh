de446e3aeeb67f0a3a6e6cc8432e983b8594500c
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2021-01-20 08:33:58 +0100

    add empty submodules

--- fetch_milk_dev.sh
+++ fetch_milk_dev.sh
@@ -1,6 +1,11 @@
 mkdir -p extra
 cd extra
-for mod in fft ZernikePolyn cudacomp image_basic image_filter image_format image_gen img_reduce info kdtree linARfilterPred linopt_imtools psf statistic 
+for mod in fft ZernikePolyn cudacomp image_basic image_filter image_format image_gen img_reduce info kdtree linARfilterPred linopt_imtools psf statistic
 do
 git clone -b dev https://github.com/milk-org/${mod}
+cat >> ../.gitmodules << EOF
+[submodule "extra/${mod}"]
+	path = extra/${mod}
+	url = ""
+EOF
 done
