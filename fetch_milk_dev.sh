051e1eeffa45388fd1be2afb2196ec14369ddbbc
oguyon <oliv.guyon@gmail.com>
2021-02-20 01:20:48 -1000

    skip installed modules

--- fetch_milk_dev.sh
+++ fetch_milk_dev.sh
@@ -1,11 +1,20 @@
+#!/usr/bin/env bash
+
 mkdir -p extra
 cd extra
+
 for mod in fft ZernikePolyn cudacomp image_basic image_filter image_format image_gen img_reduce info kdtree linARfilterPred linopt_imtools psf statistic
 do
-git clone -b dev https://github.com/milk-org/${mod}
-cat >> ../.gitmodules << EOF
+echo "Module ${mod}"
+if [ -d "${mod}" ]; then
+  echo "	Already installed -> skipping"
+else
+  git clone -b dev https://github.com/milk-org/${mod}
+  cat >> ../.gitmodules << EOF
 [submodule "extra/${mod}"]
 	path = extra/${mod}
 	url = ""
 EOF
+  echo ""
+fi
 done
