444472ae1a3379dec783ba0cd5638c1a2f6bd281
oguyon <oliv.guyon@gmail.com>
2021-02-20 01:20:48 -1000

    skip installed modules

--- fetch_cacao_dev.sh
+++ fetch_cacao_dev.sh
@@ -1,11 +1,20 @@
+#!/usr/bin/env bash
+
 source ./fetch_milk_dev.sh
+
 
 for mod in AOloopControl AOloopControl_DM AOloopControl_IOtools AOloopControl_PredictiveControl AOloopControl_acquireCalib AOloopControl_compTools AOloopControl_computeCalib AOloopControl_perfTest FPAOloopControl
 do
-git clone -b dev https://github.com/cacao-org/${mod}
-cat >> ../.gitmodules << EOF
+echo "Module ${mod}"
+if [ -d "${mod}" ]; then
+  echo "	Already installed -> skipping"
+else
+  git clone -b dev https://github.com/cacao-org/${mod}
+  cat >> ../.gitmodules << EOF
 [submodule "extra/${mod}"]
 	path = extra/${mod}
 	url = ""
 EOF
+  echo ""
+fi
 done
