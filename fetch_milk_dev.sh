468353cd923340684479b80a7f178265eae19dd7
Arnaud Sevin <Arnaud.Sevin@obspm.fr>
2021-02-23 13:45:17 +0100

    update script and cleanup cmakelists.txt

--- fetch_milk_dev.sh
+++ fetch_milk_dev.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p extra
-cd extra
+mkdir -p plugins/milk-extra-src
+cd plugins/milk-extra-src
 
 for mod in fft ZernikePolyn cudacomp image_basic image_filter image_format image_gen img_reduce info kdtree linARfilterPred linopt_imtools psf statistic
 do
 echo "Module ${mod}"
 if [ -d "${mod}" ]; then
-  echo "	Already installed -> skipping"
+  echo "	Already installed -> pulling"
+  (cd ${mod}; git pull)
 else
   git clone -b dev https://github.com/milk-org/${mod}
   echo ""
