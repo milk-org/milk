f8a944d1da029b87b970ed56afdf5af27d79e2ae
oguyon <oliv.guyon@gmail.com>
2021-02-24 10:47:06 -1000

    more compact printf

--- src/CLImain.c
+++ src/CLImain.c
@@ -11,20 +11,6 @@
 #include <CommandLineInterface/CLIcore.h>
 
 
-// #include <image_basic/image_basic.h>
-// #include <img_reduce/img_reduce.h>
-// #include <linopt_imtools/linopt_imtools.h>
-// #include <cudacomp/cudacomp.h>
-
-
-
-// #include <image_format/image_format.h>
-// #include <psf/psf.h>
-// #include <linARfilterPred/linARfilterPred.h>
-// #include <ZernikePolyn/ZernikePolyn.h>
-
-// #include <fft/fft.h>
-
 
 #define STYLE_BOLD    "\033[1m"
 #define STYLE_NO_BOLD "\033[22m"
@@ -53,22 +39,22 @@ int main(
 		data.quiet = 0;
 	}
 
+    char versionstring[STRINGMAXLEN_VERSIONSTRING];
+    snprintf(versionstring, STRINGMAXLEN_VERSIONSTRING, "%d.%02d.%02d%s",
+             VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_OPTION);
+
     if(data.quiet == 0)
     {
         printf(STYLE_BOLD);
-        printf("\n        Modular Image processing toolKit (milk)\n");
+        printf("\n        milk  v %s\n", versionstring);
 #ifndef NDEBUG
-        printf("        === DEBUG MODE : assert()         enabled ==========\n");
-        printf("        === DEBUG MODE : DEBUG_TRACEPOINT enabled ==========\n");
+        printf("        === DEBUG MODE : assert() & DEBUG_TRACEPOINT  enabled ===\n");
 #endif
         printf(STYLE_NO_BOLD);
     }
 
     strcpy(data.package_name, PACKAGE_NAME);
 
-    char versionstring[STRINGMAXLEN_VERSIONSTRING];
-    snprintf(versionstring, STRINGMAXLEN_VERSIONSTRING, "%d.%02d.%02d%s",
-             VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_OPTION);
 
     data.package_version_major = VERSION_MAJOR;
     data.package_version_minor = VERSION_MINOR;
@@ -83,33 +69,15 @@ int main(
 
     if(data.quiet == 0)
     {
-        printf("        %s version %s\n", data.package_name, data.package_version);
+        //printf("        %s version %s\n", data.package_name, data.package_version);
 #ifdef IMAGESTRUCT_VERSION
-        printf("        ImageStreamIO version %s\n", IMAGESTRUCT_VERSION);
+        printf("        ImageStreamIO v %s\n", IMAGESTRUCT_VERSION);
 #endif
         //printf("        GNU General Public License v3.0\n");
         //printf("        Report bugs to : %s\n", PACKAGE_BUGREPORT);
         //printf("        Type \"help\" for instructions\n");
         printf("        \n");
     }
-
-    // // initialize milk modules for which no function calls is included by default
-    // libinit_fft();
-
-
-    // libinit_image_basic();
-    // libinit_img_reduce();
-    // libinit_linopt_imtools();
-    // libinit_cudacomp();
-
-
-
-    // libinit_image_format();
-    // libinit_psf();
-    // libinit_linARfilterPred();
-    // libinit_ZernikePolyn();
-
-
 
     runCLI(argc, argv, AppName);
 
