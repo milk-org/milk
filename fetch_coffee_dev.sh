f75086bb60f1bcc8d4b349fc3278f3a15c71d1bd
oguyon <oliv.guyon@gmail.com>
2021-03-08 23:53:39 -1000

    add WFpropagate

--- fetch_coffee_dev.sh
+++ fetch_coffee_dev.sh
@@ -2,6 +2,21 @@
 
 ./fetch_milk_dev.sh
 
+
+for mod in WFpropagate
+do
+echo "Module ${mod}"
+if [ -d "${mod}" ]; then
+  echo "	Already installed -> pulling"
+  (cd ${mod}; git pull)
+else
+  git clone -b dev https://github.com/milk-org/${mod}
+  echo ""
+fi
+done
+
+
+
 echo "COFFEE modules"
 if [ -d "plugins/coffee-src" ]; then
   echo "	Already installed -> pulling"
