e86eadb2629fa6446ab2c9fdba6639a6f85b03e0
oguyon <oliv.guyon@gmail.com>
2021-03-25 01:01:31 -1000

    simple stream loop

--- src/milk_module_example/updatestreamloop.c
+++ src/milk_module_example/updatestreamloop.c
@@ -1,5 +1,5 @@
 /**
- * @file    updatestreamloop_brief.c
+ * @file    updatestreamloop.c
  * @brief   simple procinfo+fps example - brief, no comments, uses macros
  *
  */
@@ -34,21 +34,14 @@ static CLICMDDATA CLIcmddata =
 
 
 
-
-
-
 // Wrapper function, used by all CLI calls
-// Defines how local variables are fed to computation code
-// Always local to this translation unit
 static errno_t compute_function()
 {
     IMGID img = makeIMGID(inimname);
     resolveIMGID(&img, ERRMODE_ABORT);
 
     INSERT_STD_PROCINFO_COMPUTEFUNC_START
-    //printf("ID : %ld\n", img.ID);
     processinfo_update_output_stream(processinfo, img.ID);
-
     INSERT_STD_PROCINFO_COMPUTEFUNC_END
 
     return RETURN_SUCCESS;
@@ -64,81 +57,5 @@ errno_t FPSCLIADDCMD_milk_module_example__updatestreamloop()
 {
     INSERT_STD_FPSCLIREGISTERFUNC
 
-    // Optional custom settings for this function
-    // CLIcmddata.cmdsettings->procinfo_loopcntMax = 9;
-
     return RETURN_SUCCESS;
 }
-
-
-
-
-
-
-
-
-
-
-
-/** @brief FPCONF function
- */
-/*static errno_t FPSCONFfunction()
-{
-    FPS_SETUP_INIT(data.FPS_name, data.FPS_CMDCODE);
-    fps_add_processinfo_entries(&fps);
-    CMDargs_to_FPSparams_create(&fps);
-
-    long fps_delayus = functionparameter_GetParamValue_INT64(&fps, ".delayus");
-
-    FPS_CONFLOOP_START
-
-    printf("delayus value inside conf loop: %ld\n", fps_delayus);
-
-    FPS_CONFLOOP_END
-
-    return RETURN_SUCCESS;
-}*/
-
-
-
-/** @brief Loop process code example
- */
-/*static errno_t FPSRUNfunction()
-{
-    FPS_CONNECT(data.FPS_name, FPSCONNECT_RUN);
-
-    char *fps_IDin_name = functionparameter_GetParamPtr_STRING(&fps, ".in_name");
-    long fps_delayus = functionparameter_GetParamValue_INT64(&fps, ".delayus");
-
-    FPSPROCINFOLOOP_RUNINIT("streamupdate %.10s", fps_IDin_name);
-
-    imageID IDin = image_ID(fps_IDin_name);
-
-    PROCINFO_TRIGGER_DELAYUS(fps_delayus);
-
-
-
-    PROCINFOLOOP_START
-
-    processinfo_update_output_stream(processinfo, IDin);
-
-    PROCINFOLOOP_END
-    function_parameter_RUNexit(&fps);
-
-    return RETURN_SUCCESS;
-}
-
-
-FPS_EXECFUNCTION_STD // macro defined in function_parameters.h
-FPS_CLIFUNCTION_STD  // macro defined in function_parameters.h
-
-errno_t FPSCLIADDCMD_milk_module_example__updatestreamloop_brief()
-{
-    RegisterCLIcmd(CLIcmddata, FPSCLIfunction);
-    return RETURN_SUCCESS;
-}
-*/
-
-
-
-
