a8d141d545b4c17a95fa5d926bfbaf3af37e933c
oguyon <oliv.guyon@gmail.com>
2021-04-15 11:56:07 -1000

    formatting

--- src/milk_module_example/examplefunc1.h
+++ src/milk_module_example/examplefunc1.h
@@ -1,5 +1,5 @@
-#ifndef MILK_MODULE_EXAMPLEFUNC1_H
-#define MILK_MODULE_EXAMPLEFUNC1_H
+#ifndef MILK_MODULE_EXAMPLE_FUNC1_H
+#define MILK_MODULE_EXAMPLE_FUNC1_H
 
 errno_t CLIADDCMD_milk_module_example__simplefunc();
 
