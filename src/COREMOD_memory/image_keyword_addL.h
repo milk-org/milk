9a0f85fa45f41e8162306f6bf3cd6120816fdb23
oguyon <oliv.guyon@gmail.com>
2021-04-18 14:28:33 -1000

    keyword handling

--- src/COREMOD_memory/image_keyword_addL.h
+++ src/COREMOD_memory/image_keyword_addL.h
@@ -1,6 +1,13 @@
 #ifndef COREMOD_MEMORY_IMAGE_KEYWORD_ADDL_H
 #define COREMOD_MEMORY_IMAGE_KEYWORD_ADDL_H
 
+errno_t image_keyword_addL(
+    IMGID img,
+    char *kwname,
+    long kwval,
+    char *comment
+);
+
 errno_t CLIADDCMD_COREMOD_memory__image_keyword_addL();
 
 #endif
