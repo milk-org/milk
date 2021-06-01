cb629f1f16065dd059ffe1057618467ac14d93e9
Olivier Guyon <oliv.guyon@gmail.com>
2021-05-31 19:35:24 -1000

    set shell to bash in tmux sessions

--- src/CommandLineInterface/fps_tmux.c
+++ src/CommandLineInterface/fps_tmux.c
@@ -1,8 +1,8 @@
 /**
  * @file    fps_tmux.c
- * 
+ *
  * @brief   tmux session management
- * 
+ *
  * tmux wrapper for FPS control
  */
 
@@ -23,9 +23,9 @@ errno_t functionparameter_FPS_tmux_kill(
                            fps->md->name);
     EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:ctrl \"exit\" C-m 2> /dev/null",
                            fps->md->name);
-                           
+
     EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:conf C-c 2> /dev/null",
-                           fps->md->name);                           
+                           fps->md->name);
     EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:conf \"exit\" C-m 2> /dev/null",
                            fps->md->name);
 
@@ -35,7 +35,7 @@ errno_t functionparameter_FPS_tmux_kill(
                            fps->md->name);
 
 
-	EXECUTE_SYSTEM_COMMAND("tmux kill-session -t %s 2> /dev/null", fps->md->name);
+    EXECUTE_SYSTEM_COMMAND("tmux kill-session -t %s 2> /dev/null", fps->md->name);
 
     return RETURN_SUCCESS;
 }
@@ -51,10 +51,10 @@ errno_t functionparameter_FPS_tmux_init(
     FUNCTION_PARAMETER_STRUCT *fps
 )
 {
-	int funcstring_maxlen = 10000;
-	int argstring_maxlen = 1000;
-	int mloadstring_maxlen = 2000;
-	
+    int funcstring_maxlen = 10000;
+    int argstring_maxlen = 1000;
+    int mloadstring_maxlen = 2000;
+
     // terminate tmux sessions
     functionparameter_FPS_tmux_kill(fps);
 
@@ -74,35 +74,42 @@ errno_t functionparameter_FPS_tmux_init(
     char argstring[argstring_maxlen];
     char argstringcp[argstring_maxlen];
 
-	if(fps->md->NBnameindex > 0)
-	{		
-		snprintf(argstring, argstring_maxlen, "%s", fps->md->nameindexW[0]);
-	}
-	else
-	{
-		sprintf(argstring, " ");
-	}
-	
-	for(int i=1; i<fps->md->NBnameindex; i++)
-	{
-		snprintf(argstringcp, argstring_maxlen, "%s %s", argstring, fps->md->nameindexW[i]);
-		strcpy(argstring, argstringcp);
-	}
+    if(fps->md->NBnameindex > 0)
+    {
+        snprintf(argstring, argstring_maxlen, "%s", fps->md->nameindexW[0]);
+    }
+    else
+    {
+        sprintf(argstring, " ");
+    }
+
+    for(int i=1; i<fps->md->NBnameindex; i++)
+    {
+        snprintf(argstringcp, argstring_maxlen, "%s %s", argstring, fps->md->nameindexW[i]);
+        strcpy(argstring, argstringcp);
+    }
 
 
-	// module load string
-	char mloadstring[mloadstring_maxlen];
-	char mloadstringcp[mloadstring_maxlen];
-	sprintf(mloadstring, " ");
-	for(int m=0; m < fps->md->NBmodule; m++)
-	{
-		snprintf(mloadstringcp, mloadstring_maxlen, "%smload %s;", mloadstring, fps->md->modulename[m]);
-		strcpy(mloadstring, mloadstringcp);
-	}
-	
+    // module load string
+    char mloadstring[mloadstring_maxlen];
+    char mloadstringcp[mloadstring_maxlen];
+    sprintf(mloadstring, " ");
+    for(int m=0; m < fps->md->NBmodule; m++)
+    {
+        snprintf(mloadstringcp, mloadstring_maxlen, "%smload %s;", mloadstring, fps->md->modulename[m]);
+        strcpy(mloadstring, mloadstringcp);
+    }
 
-	// confstart
-	//
+
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:ctrl \"bash\" C-m",
+                           fps->md->name);
+
+
+    // confstart
+    //
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:conf \"bash\" C-m",
+                           fps->md->name);
+
     sprintf(functionstring,
             "function fpsconfstart {\n"
             "echo \"STARTING CONF PROCESS\"\n"
@@ -115,13 +122,16 @@ errno_t functionparameter_FPS_tmux_init(
             argstring
            );
 
-    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:conf \"%s\" C-m", 
-		fps->md->name,
-		functionstring);
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:conf \"%s\" C-m",
+                           fps->md->name,
+                           functionstring);
 
-    
+
     // runstart
     //
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run \"bash\" C-m",
+                           fps->md->name);
+
     sprintf(functionstring,
             "function fpsrunstart {\n"
             "echo \"STARTING RUN PROCESS\"\n"
@@ -134,11 +144,11 @@ errno_t functionparameter_FPS_tmux_init(
             argstring
            );
 
-    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run \"%s\" C-m", 
-		fps->md->name,
-		functionstring);    
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run \"%s\" C-m",
+                           fps->md->name,
+                           functionstring);
 
-    
+
     // runstop
     //
     sprintf(functionstring,
@@ -153,10 +163,10 @@ errno_t functionparameter_FPS_tmux_init(
             argstring
            );
 
-    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run \"%s\" C-m", 
-		fps->md->name,
-		functionstring);    
-    
+    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run \"%s\" C-m",
+                           fps->md->name,
+                           functionstring);
+
 
 
     return RETURN_SUCCESS;
