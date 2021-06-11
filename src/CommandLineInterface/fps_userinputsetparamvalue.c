5155a937de1c7902c28280e585f59ea85e42d988
Olivier Guyon <oliv.guyon@gmail.com>
2021-06-11 02:37:11 -1000

    run procCTRL screen if no process

--- src/CommandLineInterface/fps_userinputsetparamvalue.c
+++ src/CommandLineInterface/fps_userinputsetparamvalue.c
@@ -48,7 +48,7 @@ int functionparameter_UserInputSetParamValue(
 
             int stringindex = 0;
 
-			c = get_singlechar_block();
+            c = get_singlechar_block();
 
             while((c != 27) && (c != 10) && (c != 13) && (stringindex < strlenmax - 1))
             {
@@ -61,7 +61,7 @@ int functionparameter_UserInputSetParamValue(
                     stringindex --;
                 }
                 else
-                {					
+                {
                     putchar(c);  // echo on screen
                     fflush(stdout);
                     stringindex++;
@@ -70,8 +70,8 @@ int functionparameter_UserInputSetParamValue(
                 {
                     stringindex = 0;
                 }
-                
-				c = get_singlechar_block();
+
+                c = get_singlechar_block();
             }
             buff[stringindex] = '\0';
             inputOK = 1;
@@ -88,168 +88,168 @@ int functionparameter_UserInputSetParamValue(
             switch(fpsentry->parray[pindex].type)
             {
 
-                case FPTYPE_INT64:
-                    errno = 0;    /* To distinguish success/failure after call */
-                    lval = strtol(buff, &endptr, 10);
+            case FPTYPE_INT64:
+                errno = 0;    /* To distinguish success/failure after call */
+                lval = strtol(buff, &endptr, 10);
 
-                    /* Check for various possible errors */
-                    if((errno == ERANGE && (lval == LONG_MAX || lval == LONG_MIN))
-                            || (errno != 0 && lval == 0))
-                    {
-                        perror("strtol");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                /* Check for various possible errors */
+                if((errno == ERANGE && (lval == LONG_MAX || lval == LONG_MIN))
+                        || (errno != 0 && lval == 0))
+                {
+                    perror("strtol");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(endptr == buff)
-                    {
-                        fprintf(stderr, "\nERROR: No digits were found\n");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                if(endptr == buff)
+                {
+                    fprintf(stderr, "\nERROR: No digits were found\n");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(vOK == 1)
-                    {
-                        fpsentry->parray[pindex].val.i64[0] = lval;
-                    }
-                    break;
+                if(vOK == 1)
+                {
+                    fpsentry->parray[pindex].val.i64[0] = lval;
+                }
+                break;
 
-                case FPTYPE_FLOAT64:
-                    errno = 0;    /* To distinguish success/failure after call */
-                    fval = strtod(buff, &endptr);
+            case FPTYPE_FLOAT64:
+                errno = 0;    /* To distinguish success/failure after call */
+                fval = strtod(buff, &endptr);
 
-                    /* Check for various possible errors */
-                    if((errno == ERANGE)
-                            || (errno != 0 && fval == 0.0))
-                    {
-                        perror("strtod");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                /* Check for various possible errors */
+                if((errno == ERANGE)
+                        || (errno != 0 && fval == 0.0))
+                {
+                    perror("strtod");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(endptr == buff)
-                    {
-                        fprintf(stderr, "\nERROR: No digits were found\n");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                if(endptr == buff)
+                {
+                    fprintf(stderr, "\nERROR: No digits were found\n");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(vOK == 1)
-                    {
-                        fpsentry->parray[pindex].val.f64[0] = fval;
-                    }
-                    break;
-
-
-                case FPTYPE_FLOAT32:
-                    errno = 0;    /* To distinguish success/failure after call */
-                    fval = strtod(buff, &endptr);
-
-                    /* Check for various possible errors */
-                    if((errno == ERANGE)
-                            || (errno != 0 && fval == 0.0))
-                    {
-                        perror("strtod");
-                        vOK = 0;
-                        sleep(1);
-                    }
-
-                    if(endptr == buff)
-                    {
-                        fprintf(stderr, "\nERROR: No digits were found\n");
-                        vOK = 0;
-                        sleep(1);
-                    }
-
-                    if(vOK == 1)
-                    {
-                        fpsentry->parray[pindex].val.f32[0] = fval;
-                    }
-                    break;
+                if(vOK == 1)
+                {
+                    fpsentry->parray[pindex].val.f64[0] = fval;
+                }
+                break;
 
 
-                case FPTYPE_PID :
-                    errno = 0;    /* To distinguish success/failure after call */
-                    lval = strtol(buff, &endptr, 10);
+            case FPTYPE_FLOAT32:
+                errno = 0;    /* To distinguish success/failure after call */
+                fval = strtod(buff, &endptr);
 
-                    /* Check for various possible errors */
-                    if((errno == ERANGE && (lval == LONG_MAX || lval == LONG_MIN))
-                            || (errno != 0 && lval == 0))
-                    {
-                        perror("strtol");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                /* Check for various possible errors */
+                if((errno == ERANGE)
+                        || (errno != 0 && fval == 0.0))
+                {
+                    perror("strtod");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(endptr == buff)
-                    {
-                        fprintf(stderr, "\nERROR: No digits were found\n");
-                        vOK = 0;
-                        sleep(1);
-                    }
+                if(endptr == buff)
+                {
+                    fprintf(stderr, "\nERROR: No digits were found\n");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                    if(vOK == 1)
-                    {
-                        fpsentry->parray[pindex].val.pid[0] = (pid_t) lval;
-                    }
-                    break;
+                if(vOK == 1)
+                {
+                    fpsentry->parray[pindex].val.f32[0] = fval;
+                }
+                break;
 
 
-                case FPTYPE_FILENAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+            case FPTYPE_PID :
+                errno = 0;    /* To distinguish success/failure after call */
+                lval = strtol(buff, &endptr, 10);
 
-                case FPTYPE_FITSFILENAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+                /* Check for various possible errors */
+                if((errno == ERANGE && (lval == LONG_MAX || lval == LONG_MIN))
+                        || (errno != 0 && lval == 0))
+                {
+                    perror("strtol");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                case FPTYPE_EXECFILENAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+                if(endptr == buff)
+                {
+                    fprintf(stderr, "\nERROR: No digits were found\n");
+                    vOK = 0;
+                    sleep(1);
+                }
 
-                case FPTYPE_DIRNAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+                if(vOK == 1)
+                {
+                    fpsentry->parray[pindex].val.pid[0] = (pid_t) lval;
+                }
+                break;
 
-                case FPTYPE_STREAMNAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
 
-                case FPTYPE_STRING :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+            case FPTYPE_FILENAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
 
-                case FPTYPE_FPSNAME :
-                    if(snprintf(fpsentry->parray[pindex].val.string[0],
-                                FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
-                    {
-                        PRINT_ERROR("snprintf error");
-                    }
-                    break;
+            case FPTYPE_FITSFILENAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
+
+            case FPTYPE_EXECFILENAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
+
+            case FPTYPE_DIRNAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
+
+            case FPTYPE_STREAMNAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
+
+            case FPTYPE_STRING :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
+
+            case FPTYPE_FPSNAME :
+                if(snprintf(fpsentry->parray[pindex].val.string[0],
+                            FUNCTION_PARAMETER_STRMAXLEN, "%s", buff) < 0)
+                {
+                    PRINT_ERROR("snprintf error");
+                }
+                break;
 
             }
 
