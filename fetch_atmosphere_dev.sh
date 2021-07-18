915810a161c854e199bdeeac044a144d400abd44
Olivier Guyon <oliv.guyon@gmail.com>
2021-07-17 19:57:16 -1000

    add atmosphere simulation modules

---
+++ fetch_atmosphere_dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+./fetch_milk_dev.sh
+
+CWD=$PWD
+cd plugins/milk-extra-src
+for mod in WFpropagate OpticsMaterials AtmosphereModel AtmosphericTurbulence
+do
+echo "Module ${mod}"
+if [ -d "${mod}" ]; then
+  echo "	Already installed -> pulling"
+  (cd ${mod}; git pull)
+else
+  git clone -b dev https://github.com/milk-org/${mod}
+  echo ""
+fi
+done
+cd $CWD
