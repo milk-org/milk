06b730cd8535b2b17bbd4ed6326012b6ca4f6be6
vdeo <vdeo@naoj.org>
2021-08-19 12:58:41 -1000

    New function+scripts move any PID to cpuset, elevate to RT prio

--- src/COREMOD_tools/mvprocCPUset.h
+++ src/COREMOD_tools/mvprocCPUset.h
@@ -3,5 +3,7 @@
  */
 
 errno_t mvprocCPUset_addCLIcmd();
+errno_t mvprocCPUsetExt_addCLIcmd();
 
 int COREMOD_TOOLS_mvProcCPUset(const char *csetname);
+int COREMOD_TOOLS_mvProcCPUsetExt(int pid, const char *csetname, int rtprio);
