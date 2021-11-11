1b1d784bf70aa70c2d6af9e48243a2b4162b708a
Olivier Guyon <oliv.guyon@gmail.com>
2021-11-10 18:05:06 -1000

    imLOC test

--- src/COREMOD_iofits/loadmemstream.c
+++ src/COREMOD_iofits/loadmemstream.c
@@ -371,6 +371,10 @@ imageID COREMOD_IOFITS_LoadMemStream(
             }
             printf("%s %d imLOC = %d\n", __FILE__, __LINE__, *imLOC);//TBE
             printf("image name : \"%s\"\n", sname);
+            if(sname == NULL)
+            {
+                printf("NULL pointer\n");
+            }
             ID = read_sharedmem_image(sname);
             printf("%s %d imLOC = %d\n", __FILE__, __LINE__, *imLOC);//TBE
             if(ID != -1)
