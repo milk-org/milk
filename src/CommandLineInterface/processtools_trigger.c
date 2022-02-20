eb75c9d2ac2b550b81f6f2ba03983ad9543c0a6e
Olivier Guyon <oliv.guyon@gmail.com>
2022-02-19 16:23:29 -1000

    set timeout

--- src/CommandLineInterface/processtools_trigger.c
+++ src/CommandLineInterface/processtools_trigger.c
@@ -144,10 +144,6 @@ errno_t processinfo_waitoninputstream_init(PROCESSINFO *processinfo,
         }
     }
 
-    // set default timeout to 2 sec
-    processinfo->triggertimeout.tv_sec  = 2;
-    processinfo->triggertimeout.tv_nsec = 0;
-
     DEBUG_TRACE_FEXIT();
     return RETURN_SUCCESS;
 }
@@ -277,8 +273,8 @@ errno_t processinfo_waitoninputstream(PROCESSINFO *processinfo)
         {
             DEBUG_TRACEPOINT("timedwait");
             // add timeout
-            ts.tv_sec += 0;        //processinfo->triggertimeout.tv_sec;
-            ts.tv_nsec += 1000000; //processinfo->triggertimeout.tv_nsec;
+            ts.tv_sec += processinfo->triggertimeout.tv_sec;
+            ts.tv_nsec += processinfo->triggertimeout.tv_nsec;
             while (ts.tv_nsec > 1000000000)
             {
                 ts.tv_nsec -= 1000000000;
