bdbd704a85f3cfa2fbae7eface2f3c3aa8239457
Olivier Guyon <oliv.guyon@gmail.com>
2022-02-19 16:27:01 -1000

    fit timeout

--- src/CommandLineInterface/CLIcore_utils.h
+++ src/CommandLineInterface/CLIcore_utils.h
@@ -229,8 +229,8 @@ typedef struct
     int          processloopOK = 1;                                            \
     PROCESSINFO *processinfo   = NULL;                                         \
     /* set default timeout to 2 sec */                                         \
-    processinfo->triggertimeout.tv_sec  = 2;                                   \
-    processinfo->triggertimeout.tv_nsec = 0;                                   \
+    CLIcmddata.cmdsettings->triggertimeout.tv_sec  = 2;                        \
+    CLIcmddata.cmdsettings->triggertimeout.tv_nsec = 0;                        \
     if (data.fpsptr != NULL)                                                   \
     { /* If FPS mode, then FPS settings override defaults*/                    \
         /* data.fpsptr->cmset entries are read by fps_connect */               \
