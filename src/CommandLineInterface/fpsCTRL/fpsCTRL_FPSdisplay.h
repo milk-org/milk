60339d6322365fd2809eca34bc099275758b0056
Olivier Guyon <oliv.guyon@gmail.com>
2022-05-29 00:15:31 -1000

    fpsCTRL cleanup

---
+++ src/CommandLineInterface/fpsCTRL/fpsCTRL_FPSdisplay.h
@@ -0,0 +1,8 @@
+#ifndef FPS_CTRLSCREEN_FPSDISPLAY_H
+#define FPS_CTRLSCREEN_FPSDISPLAY_H
+
+
+errno_t fpsCTRL_FPSdisplay(KEYWORD_TREE_NODE    *keywnode,
+                           FPSCTRL_PROCESS_VARS *fpsCTRLvar);
+
+#endif
---
+++ src/CommandLineInterface/fpsCTRL/print_nodeinfo.h
@@ -0,0 +1,10 @@
+#ifndef FPS_CTRLSCREEN_PRINT_NODEINFO_H
+#define FPS_CTRLSCREEN_PRINT_NODEINFO_H
+
+void fpsCTRLscreen_print_nodeinfo(FUNCTION_PARAMETER_STRUCT *fps,
+                                  KEYWORD_TREE_NODE         *keywnode,
+                                  int                        nodeSelected,
+                                  int                        fpsindexSelected,
+                                  int                        pindexSelected);
+
+#endif
