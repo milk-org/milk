4ac7a6c30a1523b52689212404913933aaf07bd1
vdeo <vdeo@naoj.org>
2022-08-22 16:30:19 -1000

    Temp: partially working UDP stream for single DGRAM stream (<65kB)

--- src/COREMOD_memory/stream_UDP.h
+++ src/COREMOD_memory/stream_UDP.h
@@ -5,19 +5,15 @@
 #ifndef _STREAM_TCP_H
 #define _STREAM_TCP_H
 
-errno_t stream__TCP_addCLIcmd();
+errno_t stream__UDP_addCLIcmd();
 
-errno_t COREMOD_MEMORY_testfunction_semaphore(const char *IDname,
-                                              int         semtrig,
-                                              int         testmode);
-
-imageID COREMOD_MEMORY_image_NETWORKtransmit(const char *IDname,
+imageID COREMOD_MEMORY_image_NETUDPtransmit(const char *IDname,
                                              const char *IPaddr,
                                              int         port,
                                              int         mode,
                                              int         RT_priority);
 
 imageID
-COREMOD_MEMORY_image_NETWORKreceive(int port, int mode, int RT_priority);
+COREMOD_MEMORY_image_NETUDPreceive(int port, int mode, int RT_priority);
 
 #endif
