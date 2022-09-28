3d457163d9784da0697c0e37fac5faacbeee778d
Olivier Guyon <oliv.guyon@gmail.com>
2022-09-27 22:52:25 -1000

    avoid overflow

--- src/COREMOD_memory/image_keyword_addS.c
+++ src/COREMOD_memory/image_keyword_addS.c
@@ -5,39 +5,54 @@ static char *kwname;
 static char *kwval;
 static char *comment;
 
-static CLICMDARGDEF farg[] = {{CLIARG_IMG,
-                               ".in_name",
-                               "input image",
-                               "im1",
-                               CLIARG_VISIBLE_DEFAULT,
-                               (void **) &inimname,
-                               NULL},
-                              {CLIARG_STR,
-                               ".kwname",
-                               "keyword name",
-                               "KW1",
-                               CLIARG_VISIBLE_DEFAULT,
-                               (void **) &kwname,
-                               NULL},
-                              {CLIARG_STR,
-                               ".kwval",
-                               "keyword value",
-                               "blue",
-                               CLIARG_VISIBLE_DEFAULT,
-                               (void **) &kwval,
-                               NULL},
-                              {CLIARG_STR,
-                               ".comment",
-                               "comment",
-                               "keyword comment",
-                               CLIARG_VISIBLE_DEFAULT,
-                               (void **) &comment,
-                               NULL}};
+static CLICMDARGDEF farg[] = {{
+        CLIARG_IMG,
+        ".in_name",
+        "input image",
+        "im1",
+        CLIARG_VISIBLE_DEFAULT,
+        (void **) &inimname,
+        NULL
+    },
+    {   CLIARG_STR,
+        ".kwname",
+        "keyword name",
+        "KW1",
+        CLIARG_VISIBLE_DEFAULT,
+        (void **) &kwname,
+        NULL
+    },
+    {   CLIARG_STR,
+        ".kwval",
+        "keyword value",
+        "blue",
+        CLIARG_VISIBLE_DEFAULT,
+        (void **) &kwval,
+        NULL
+    },
+    {   CLIARG_STR,
+        ".comment",
+        "comment",
+        "keyword comment",
+        CLIARG_VISIBLE_DEFAULT,
+        (void **) &comment,
+        NULL
+    }
+};
 
 static CLICMDDATA CLIcmddata = {
-    "imkwaddS", "add string type image keyword", CLICMD_FIELDS_NOFPS};
+    "imkwaddS", "add string type image keyword", CLICMD_FIELDS_NOFPS
+};
 
-errno_t image_keyword_addS(IMGID img, char *kwname, char *kwval, char *comment)
+
+
+
+errno_t image_keyword_addS(
+    IMGID img,
+    char *kwname,
+    char *kwval,
+    char *comment
+)
 {
     resolveIMGID(&img, ERRMODE_ABORT);
 
@@ -57,14 +72,18 @@ errno_t image_keyword_addS(IMGID img, char *kwname, char *kwval, char *comment)
     }
     else
     {
-        strcpy(img.im->kw[kw].name, kwname);
+        //printf("writing kw %3d / %3d  \"%s\"  %d %d %d\n", kw, NBkw, kwname, strlen(kwname), strlen(kwval), strlen(comment));
+
+        strncpy(img.im->kw[kw].name, kwname, KEYWORD_MAX_STRING);
         img.im->kw[kw].type = 'S';
-        strcpy(img.im->kw[kw].value.valstr, kwval);
-        strcpy(img.im->kw[kw].comment, comment);
+        strncpy(img.im->kw[kw].value.valstr, kwval, KEYWORD_MAX_STRING);
+        strncpy(img.im->kw[kw].comment, comment, KEYWORD_MAX_COMMENT);
     }
 
     return RETURN_SUCCESS;
 }
+
+
 
 static errno_t compute_function()
 {
@@ -74,8 +93,8 @@ static errno_t compute_function()
 
 INSERT_STD_CLIfunction
 
-    errno_t
-    CLIADDCMD_COREMOD_memory__image_keyword_addS()
+errno_t
+CLIADDCMD_COREMOD_memory__image_keyword_addS()
 {
     INSERT_STD_CLIREGISTERFUNC
 
