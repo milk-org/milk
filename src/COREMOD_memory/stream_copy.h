a6a40d2ee51d47c4d2c8a4b9b58691a409d01600
Olivier Guyon <oliv.guyon@gmail.com>
2022-10-08 03:46:17 -1000

    add stream copy

---
+++ src/COREMOD_memory/stream_copy.h
@@ -0,0 +1,10 @@
+/**
+ * @file    stream_copy.h
+ */
+
+#ifndef MILK_COREMOD_MEMORY_STREAM_COPY_H
+#define MILK_COREMOD_MEMORY_STREAM_COPY_H
+
+errno_t CLIADDCMD_COREMOD_memory__stream_copy();
+
+#endif
