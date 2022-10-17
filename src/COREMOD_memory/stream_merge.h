d253c146a94a4eb03ad73cf10d8a6d25e0a0e2a0
vdeo <vdeo@naoj.org>
2022-10-17 12:24:57 -1000

    stream_merge basics

---
+++ src/COREMOD_memory/stream_merge.h
@@ -0,0 +1,10 @@
+/**
+ * @file    stream_copy.h
+ */
+
+#ifndef MILK_COREMOD_MEMORY_STREAM_MERGE_H
+#define MILK_COREMOD_MEMORY_STREAM_MERGE_H
+
+errno_t CLIADDCMD_COREMOD_memory__stream_merge();
+
+#endif // MILK_COREMOD_MEMORY_STREAM_MERGE_H
