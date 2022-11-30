64e2fe54f8a212e9948dbf5b0693723188bf1e38
Olivier Guyon <oliv.guyon@gmail.com>
2022-11-30 09:33:02 -1000

    sync

--- src/CommandLineInterface/CLIcore/CLIcore_utils.h
+++ src/CommandLineInterface/CLIcore/CLIcore_utils.h
@@ -930,7 +930,11 @@ static inline uint64_t IMGIDcompare(IMGID img, IMGID imgtemplate)
  * @return IMGID
  */
 static inline IMGID
-stream_connect_create_2Df32(char *imname, uint32_t xsize, uint32_t ysize)
+stream_connect_create_2Df32(
+    char *imname,
+    uint32_t xsize,
+    uint32_t ysize
+)
 {
     IMGID img = mkIMGID_from_name(imname);
     resolveIMGID(&img, ERRMODE_WARN);
@@ -964,7 +968,7 @@ stream_connect_create_2Df32(char *imname, uint32_t xsize, uint32_t ysize)
         arraytmp[0] = xsize;
         arraytmp[1] = ysize;
 
-        create_image_ID(imname, 2, arraytmp, _DATATYPE_FLOAT, 1, 0, 0, &img.ID);
+        create_image_ID(imname, 2, arraytmp, _DATATYPE_FLOAT, 1, 10, 0, &img.ID);
         free(arraytmp);
     }
 
