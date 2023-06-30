e6110e5955324e4d5c1e957fef503d6ca5c2067f
oguyon <oliv.guyon@gmail.com>
2023-06-30 09:31:12 -1000

    FPS output logging made optional for debugging

--- src/CommandLineInterface/fps/fps_outlog.h
+++ src/CommandLineInterface/fps/fps_outlog.h
@@ -6,12 +6,26 @@
 #ifndef FPS_OUTLOG_H
 #define FPS_OUTLOG_H
 
-errno_t getFPSlogfname(char *logfname);
+errno_t getFPSlogfname(
+    char *logfname
+);
 
-errno_t functionparameter_outlog_file(char *keyw, char *msgstring, FILE *fpout);
 
-errno_t functionparameter_outlog(char *keyw, const char *fmt, ...);
+errno_t functionparameter_outlog_file(
+    char *keyw,
+    char *msgstring,
+    FILE *fpout
+);
+
+
+
+errno_t functionparameter_outlog(
+    char *keyw,
+    const char *fmt, ...
+);
+
 
 errno_t functionparameter_outlog_namelink();
+
 
 #endif
