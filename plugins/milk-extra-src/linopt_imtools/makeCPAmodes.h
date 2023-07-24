19fdb386ed05f28f7de6b23e8a99bcd67268bcfb
oguyon <oliv.guyon@gmail.com>
2023-07-23 18:09:57 -1000

    include all extra dirs

--- plugins/milk-extra-src/linopt_imtools/makeCPAmodes.h
+++ plugins/milk-extra-src/linopt_imtools/makeCPAmodes.h
@@ -5,6 +5,8 @@ errno_t CLIADDCMD_linopt_imtools__makeCPAmodes();
 
 errno_t linopt_imtools_makeCPAmodes(const char *ID_name,
                                     long        size,
+                                    float       rCPAmin,
+                                    float       rCPAmax,
                                     float       CPAmax,
                                     float       deltaCPA,
                                     float       radius,
