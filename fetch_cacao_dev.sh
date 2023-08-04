74acc448e51aacd0d46d08ee0efc2113bc8c9d0e
Brian Carcich <briantcarcich@gmail.com>
2023-08-04 16:34:27 -0400

    Remove lapacke library from plugins/cacao-src/ repo when using mkl-sdl

--- fetch_cacao_dev.sh
+++ fetch_cacao_dev.sh
@@ -8,3 +8,10 @@ else
     git clone -b dev https://github.com/cacao-org/cacao plugins/cacao-src
     echo ""
 fi
+
+topdir="$(dirname $0)"
+mkl_sdl=$(grep '^[\t ]*pkg_check_modules( *MKL  *mkl-sdl *) *$' "$topdir/CMakeLists.txt")
+[ "$mkl_sdl" ] \
+&& sed -i -e '/^[	 ]*lapacke$/s/lapacke/#&/' plugins/cacao-src/computeCalib/CMakeLists.txt \
+&& echo "Updated plugins/cacao-src/computeCalib/CMakeLists.txt to remove lapacke library" \
+|| true
