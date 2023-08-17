ada97a3e02c35bb134848428d9f901c620695c66
Brian T. Carcich <Brian.T.Carcich>
2023-08-17 19:10:56 -0400

    Allow CACAO to be cloned and built from branch under local file:///$HOME/githubalt/cacao/ directory

--- fetch_cacao_dev.sh
+++ fetch_cacao_dev.sh
@@ -5,7 +5,11 @@ if [ -d "plugins/cacao-src" ]; then
     echo "	Already installed -> pulling"
     (cd plugins/cacao-src; git pull)
 else
-    git clone -b dev https://github.com/cacao-org/cacao plugins/cacao-src
+    [ -d "$HOME/githubalt/cacao/.git" ] \
+    && repository="file://$HOME/githubalt/cacao" branchopt= \
+    || repository="https://github.com/cacao-org/cacao.git" branchopt="-b dev"
+    echo git clone $branchopt "$repository" plugins/cacao-src
+    git clone $branchopt "$repository" plugins/cacao-src
     echo ""
 fi
 
