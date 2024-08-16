44fc815c82685bbb9b177ee285ce8a0ede963aeb
oguyon <oliv.guyon@gmail.com>
2024-08-15 18:05:57 -1000

    verbose

--- src/COREMOD_arith/image_multicrop2D.c
+++ src/COREMOD_arith/image_multicrop2D.c
@@ -425,15 +425,18 @@ static errno_t compute_function()
             if ( *wactive[cropwindow] == 1)
             {
                 uint32_t iimax = *wcropxsize[cropwindow];
+                printf("%d  iimax %5d\n", __LINE__, iimax);
                 if ( iimax +  *wcropxpos[cropwindow] > (*outxsize))
                 {
                     iimax = (*outxsize) - *wcropxpos[cropwindow];
                 }
+                printf("%d  iimax %5d\n", __LINE__, iimax);
 
                 if ( iimax + *wcropxstart[cropwindow] > imgin.md->size[0])
                 {
                     iimax = imgin.md->size[0] - *wcropxstart[cropwindow];
                 }
+                printf("%d  iimax %5d\n", __LINE__, iimax);
 
 
                 uint32_t jjmax = *wcropysize[cropwindow];
