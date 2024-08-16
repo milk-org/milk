5e074182d11e534dbbc587c402c8ed66d8c79c90
oguyon <oliv.guyon@gmail.com>
2024-08-15 17:54:51 -1000

    less verbose

--- src/COREMOD_arith/image_multicrop2D.c
+++ src/COREMOD_arith/image_multicrop2D.c
@@ -523,7 +523,6 @@ static errno_t compute_function()
                     }
                     else  // add pixels
                     {
-                        printf("iimax %5d  jimax %5d\n", iimax, jjmax);
                         switch (imgin.md->datatype)
                         {
 
