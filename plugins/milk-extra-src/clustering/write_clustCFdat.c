1e4eb5a5aa1c046d90211f3c454798e09ff53718
oguyon <oliv.guyon@gmail.com>
2025-06-14 21:46:29 -1000

    output write funcs as separate files

---
+++ plugins/milk-extra-src/clustering/write_clustCFdat.c
@@ -0,0 +1,54 @@
+#include <math.h>
+
+#include "CommandLineInterface/CLIcore.h"
+#include "COREMOD_iofits/COREMOD_iofits.h"
+
+#include "clustering_defs.h"
+
+
+errno_t write_clustCFdat(
+    CLUSTERTREE *ctree,
+    const char *__restrict fname
+)
+{
+    DEBUG_TRACE_FSTART();
+
+    FILE *fp = fopen(fname, "w");
+
+    fprintf(fp,"# col1   CF index\n");
+    fprintf(fp,"# col2   CF type\n");
+    fprintf(fp,"# col3   CF level\n");
+    fprintf(fp,"# col4   Number of point within CF\n");
+    fprintf(fp,"# col5   NBchild\n");
+    fprintf(fp,"# col6   parent index\n");
+    fprintf(fp,"# col7   datasq\n");
+    fprintf(fp,"# col8   radius2\n");
+    fprintf(fp,"# col9   radius3/threshold\n");
+
+    for(long CFindex = 0; CFindex < ctree->NBCF; CFindex++)
+    {
+        if(ctree->CFarray[CFindex].type != CLUSTER_CF_TYPE_UNUSED)
+        {
+            fprintf(fp,
+                    "%5ld  %2d %2d  %5ld %5d %5ld  %16.3f %16.3f  %6.4f",
+                    CFindex,
+                    ctree->CFarray[CFindex].type,
+                    ctree->CFarray[CFindex].level,
+                    ctree->CFarray[CFindex].N,
+                    ctree->CFarray[CFindex].NBchild,
+                    ctree->CFarray[CFindex].parentindex,
+                    (double) ctree->CFarray[CFindex].datassq,
+                    (double) sqrt(ctree->CFarray[CFindex].radius2),
+                    (double) sqrt(ctree->CFarray[CFindex].radius2)/ctree->T
+                   );
+
+            fprintf(fp, "\n");
+        }
+
+    }
+
+    fclose(fp);
+
+    DEBUG_TRACE_FEXIT();
+    return RETURN_SUCCESS;
+}
