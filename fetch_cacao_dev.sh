1a0791fa2ff84a18057f4a8f07a24970f356734e
Joseph Long <github@joseph-long.com>
2025-10-13 14:36:24 -0400

    Add support for overriding CACAO_REPOSITORY and CACAO_BRANCH (for MagAO-X builds)

--- fetch_cacao_dev.sh
+++ fetch_cacao_dev.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-
+set -ex
 echo "Module CACAO"
 if [ -d "plugins/cacao-src" ]; then
     echo "	Already installed -> pulling"
-    (cd plugins/cacao-src; git pull)
+    pushd plugins/cacao-src
+    git pull
+    popd
 else
-    [ -d "$HOME/githubalt/cacao/.git" ] \
-    && repository="file://$HOME/githubalt/cacao" branchopt= \
-    || repository="https://github.com/cacao-org/cacao.git" branchopt="-b dev"
-    echo git clone $branchopt "$repository" plugins/cacao-src
+    # If a local mirror exists in $HOME/githubalt/cacao, use it
+    if [ -d "$HOME/githubalt/cacao/.git" ]; then
+        repository="file://$HOME/githubalt/cacao"
+        branchopt=""
+    else
+        # Allow overriding the upstream repository and branch via environment variables.
+        # CACAO_REPOSITORY - full git URL to clone (defaults to official cacao repo)
+        # CACAO_BRANCH     - branch to checkout (defaults to 'dev')
+        repository="${CACAO_REPOSITORY:-https://github.com/cacao-org/cacao.git}"
+        branch="${CACAO_BRANCH:-dev}"
+        branchopt="-b ${CACAO_BRANCH:-dev}"
+    fi
     git clone $branchopt "$repository" plugins/cacao-src
-    echo ""
 fi
-
-
