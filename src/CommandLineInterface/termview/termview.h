898349953fc8bc24ac1848bfc0f8417e3bc089de
oguyon <oliv.guyon@gmail.com>
2026-01-10 11:06:12 -1000

    Enhance milk-termview: colormaps, scaling, auto-ranging, and interactive menu

--- src/CommandLineInterface/termview/termview.h
+++ src/CommandLineInterface/termview/termview.h
@@ -4,8 +4,33 @@
 #include <stdbool.h>
 #include "CommandLineInterface/CLIcore.h"
 
+typedef enum {
+    COLORMAP_GREYSCALE = 0,
+    COLORMAP_HEAT,
+    COLORMAP_COLD,
+    COLORMAP_JET,
+    COLORMAP_NB
+} termview_colormap_t;
+
+typedef enum {
+    SCALE_LINEAR = 0,
+    SCALE_SQRT,
+    SCALE_LOG,
+    SCALE_NB
+} termview_scale_t;
+
+typedef enum {
+    RANGE_MINMAX = 0,
+    RANGE_01_99,
+    RANGE_05_95,
+    RANGE_10_90,
+    RANGE_NB
+} termview_range_t;
+
 typedef struct {
-    bool force_ascii;
+    termview_colormap_t colormap;
+    termview_scale_t scale;
+    termview_range_t range;
 } termview_options_t;
 
 errno_t termview_screen(const char *imagename, termview_options_t options);
