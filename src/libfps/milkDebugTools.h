0159c15a023819d4ad36c4019227e1f82cbe9719
oguyon <oliv.guyon@gmail.com>
2026-01-17 15:29:39 -1000

    fix(libfps): really silence unused-result warning
    Use dummy variable assignment to silence system() return value warning.

--- src/libfps/milkDebugTools.h
+++ src/libfps/milkDebugTools.h
@@ -66,7 +66,7 @@
 #define EXECUTE_SYSTEM_COMMAND(...) do { \
     char cmd[STRINGMAXLEN_COMMAND]; \
     snprintf(cmd, STRINGMAXLEN_COMMAND, __VA_ARGS__); \
-    (void)system(cmd); \
+    int __attribute__((unused)) ret = system(cmd); \
 } while(0)
 #endif
 
