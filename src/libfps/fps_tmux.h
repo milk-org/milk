b5445336125acdc33c78a6f7f476dbb2b9657cd1
oguyon <oliv.guyon@gmail.com>
2026-01-27 16:51:43 -1000

    optimizing libfps for code reuse

--- src/libfps/fps_tmux.h
+++ src/libfps/fps_tmux.h
@@ -17,4 +17,12 @@ errno_t functionparameter_FPS_tmux_init(FUNCTION_PARAMETER_STRUCT *fps);
 
 errno_t functionparameter_FPS_tmux_ensure(FUNCTION_PARAMETER_STRUCT *fps);
 
+errno_t functionparameter_FPS_tmux_standalone_setup(const char *fps_name);
+
+errno_t functionparameter_FPS_tmux_send(const char *fps_name, const char *window, const char *cmd_str);
+
+errno_t functionparameter_FPS_tmux_send_dispatch(const char *fps_name, const char *command, const char *exec_path, const char *extra_args);
+
+char* functionparameter_FPS_get_executable_path(char *buffer, size_t size);
+
 #endif
