5d6d0a7c21fa8af7144db7a770423b08a912a964
oguyon <oliv.guyon@gmail.com>
2026-01-30 17:24:55 -1000

    added FPS tools

---
+++ src/libfps/milk-fps-list.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "fps.h"
+#include "fps_globals.h"
+#include "fps_scan.h"
+
+void print_help(const char *progname) {
+    printf("Usage: %s [options]\n", progname);
+    printf("List active FPS instances.\n");
+    printf("\n");
+    printf("Options:\n");
+    printf("  -v, --verbose   Verbose mode (print search directory and details)\n");
+    printf("  -e, --exec      Show full path to executable\n");
+    printf("  -h, --help      Show this help message\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int verbose = 0;
+    int show_exec = 0;
+    int opt;
+
+    static struct option long_options[] = {
+        {"verbose", no_argument,       0, 'v'},
+        {"exec",    no_argument,       0, 'e'},
+        {"help",    no_argument,       0, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    while ((opt = getopt_long(argc, argv, "veh", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'v':
+                verbose = 1;
+                break;
+            case 'e':
+                show_exec = 1;
+                break;
+            case 'h':
+                print_help(argv[0]);
+                return 0;
+            default:
+                print_help(argv[0]);
+                return 1;
+        }
+    }
+
+    // Initialize fpsarray
+    fpsarray = (FUNCTION_PARAMETER_STRUCT *) calloc(NB_FPS_MAX, sizeof(FUNCTION_PARAMETER_STRUCT));
+    if(fpsarray == NULL)
+    {
+        fprintf(stderr, "Error: cannot allocate fpsarray\n");
+        return 1;
+    }
+    for(int i = 0; i < NB_FPS_MAX; i++)
+    {
+        fpsarray[i].SMfd = -1;
+    }
+
+    // Keywnode for scan
+    KEYWORD_TREE_NODE *keywnode = (KEYWORD_TREE_NODE *) calloc(NB_KEYWNODE_MAX, sizeof(KEYWORD_TREE_NODE));
+    if(keywnode == NULL)
+    {
+        fprintf(stderr, "Error: cannot allocate keywnode\n");
+        free(fpsarray);
+        return 1;
+    }
+
+    int NBkwn = 0;
+    int NBfps = 0;
+    long NBpindex = 0;
+
+    // Scan FPS
+    // mode 0: scan all
+    functionparameter_scan_fps(0, "_ALL", fpsarray, keywnode, &NBkwn, &NBfps, &NBpindex, verbose);
+
+    if (NBfps > 0) {
+        if (show_exec) {
+            printf("%-20s %-10s %-30s %s\n", "FPS Name", "Status", "Exec Path", "Description");
+            printf("--------------------------------------------------------------------------------------------\n");
+        } else {
+            printf("%-30s %-10s %s\n", "FPS Name", "Status", "Description");
+            printf("------------------------------------------------------------\n");
+        }
+
+        for(int i = 0; i < NBfps; i++)
+        {
+            char status_str[32] = "UNKNOWN";
+            if(fpsarray[i].md->status & FUNCTION_PARAMETER_STRUCT_STATUS_CONF)
+                strcpy(status_str, "CONF");
+            else if(fpsarray[i].md->status & FUNCTION_PARAMETER_STRUCT_STATUS_RUN)
+                strcpy(status_str, "RUN");
+            
+            if (show_exec) {
+                printf("%-20s %-10s %-30s %s\n", fpsarray[i].md->name, status_str, fpsarray[i].md->execfullpath, fpsarray[i].md->description);
+            } else {
+                printf("%-30s %-10s %s\n", fpsarray[i].md->name, status_str, fpsarray[i].md->description);
+            }
+            
+            // Disconnect to clean up
+            function_parameter_struct_disconnect(&fpsarray[i]);
+        }
+    } else {
+        if (!verbose) {
+            printf("No FPS instances found. Use -v for more details.\n");
+        }
+    }
+
+    free(keywnode);
+    free(fpsarray);
+
+    return 0;
+}
---
+++ src/libprocessinfo/quicksort.h
@@ -0,0 +1,141 @@
+/**
+ * @file quicksort.h
+ */
+
+int bubble_sort(
+    double * __restrict array,
+    unsigned long count
+);
+
+void qs_float(
+    float * __restrict array,
+    unsigned long left,
+    unsigned long right
+);
+
+void qs_long(
+    long * __restrict array,
+    unsigned long left,
+    unsigned long right
+);
+
+void qs_double(
+    double * __restrict array,
+    unsigned long left,
+    unsigned long right
+);
+
+void qs_ushort(
+    unsigned short * __restrict array,
+    unsigned long left,
+    unsigned long right
+);
+
+void quick_sort_float(
+    float * __restrict array,
+    unsigned long count
+);
+
+void quick_sort_long(
+    long * __restrict array,
+    unsigned long count
+);
+
+void quick_sort_double(
+    double * __restrict array,
+    unsigned long count
+);
+
+void quick_sort_ushort(
+    unsigned short * __restrict array,
+    unsigned long count
+);
+
+void qs2(
+    double       * __restrict array,
+    double       * __restrict array1,
+    unsigned long left,
+    unsigned long right
+);
+
+void qs3(
+    double       * __restrict array,
+    double       * __restrict array1,
+    double       * __restrict array2,
+    unsigned long left,
+    unsigned long right
+);
+
+void qs3_double(
+    double       * __restrict array,
+    double       * __restrict array1,
+    double       * __restrict array2,
+    unsigned long left,
+    unsigned long right
+);
+
+void quick_sort2(
+    double       * __restrict array,
+    double       * __restrict array1,
+    unsigned long count
+);
+
+void quick_sort3(
+    double       * __restrict array,
+    double       * __restrict array1,
+    double       * __restrict array2,
+    unsigned long count
+);
+
+void quick_sort3_float(
+    float        * __restrict array,
+    float        * __restrict array1,
+    float        * __restrict array2,
+    unsigned long count
+);
+
+void quick_sort3_double(
+    double       * __restrict array,
+    double       * __restrict array1,
+    double       * __restrict array2,
+    unsigned long count
+);
+
+void qs2l(
+    double * __restrict array,
+    long   * __restrict array1,
+    unsigned long left,
+    unsigned long right
+);
+
+void quick_sort2l(
+    double * __restrict array,
+    long   * __restrict array1,
+    unsigned long count
+);
+
+void quick_sort2l_double(
+    double * __restrict array,
+    long   * __restrict array1,
+    unsigned long count
+);
+
+void quick_sort2ul_double(
+    double        * __restrict array,
+    unsigned long * __restrict array1,
+    unsigned long  count
+);
+
+void quick_sort3ll_double(
+    double       * __restrict array,
+    long         * __restrict array1,
+    long         * __restrict array2,
+    unsigned long count
+);
+
+void quick_sort3ulul_double(
+    double        * __restrict array,
+    unsigned long * __restrict array1,
+    unsigned long * __restrict array2,
+    unsigned long  count
+);
