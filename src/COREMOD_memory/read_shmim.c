025d50e832243c0ec7665e7a91fa22521224d7e2
oguyon <oliv.guyon@gmail.com>
2026-02-01 11:56:00 -1000

    imageID h file

--- src/COREMOD_memory/read_shmim.c
+++ src/COREMOD_memory/read_shmim.c
@@ -12,6 +12,7 @@
 #include "image_ID.h"
 #include "image_keyword_list.h"
 #include "list_image.h"
+#include "imageID.h"
 
 // Local variables pointers
 static char *insname;
@@ -44,80 +45,7 @@ static errno_t help_function()
 
 
 
-imageID RegisterIMGID(
-    IMGID *img,
-    IMAGE *imagearray,
-    long NB_images
-)
-{
-    imageID ID = -1;
 
-    if (imagearray == NULL)
-    {
-        // If no array provided, we close the image and return 0 (success) or -1 (fail)
-        // This corresponds to non-CLI mode check
-        if (img->ID != -1)
-        {
-            ImageStreamIO_closeIm(img->im);
-            free(img->im);
-            img->ID = 0;
-            return 0;
-        }
-        return -1;
-    }
-
-    // Check if already loaded
-    ID = image_ID(img->name, imagearray, NB_images);
-    if(ID != -1)
-    {
-        // Already loaded: close the one we just opened and point to the existing one
-        if (img->im != NULL)
-        {
-            ImageStreamIO_closeIm(img->im);
-            free(img->im);
-        }
-        img->ID = ID;
-        img->im = &imagearray[ID];
-        img->md = &imagearray[ID].md[0];
-        img->createcnt = imagearray[ID].createcnt;
-        updateIMGIDcreationparams(img);
-    }
-    else
-    {
-        // Not loaded: find slot and move it
-        ID = next_avail_image_ID(-1);
-        if (ID != -1)
-        {
-            // We assume imagearray has enough space and ID is valid index
-            // Move content
-            memcpy(&imagearray[ID], img->im, sizeof(IMAGE));
-            // Free temporary structure
-            free(img->im);
-
-            img->ID = ID;
-            img->im = &imagearray[ID];
-            img->md = &imagearray[ID].md[0];
-            // img.createcnt = data.image[img.ID].createcnt; // Should be set? ImageStreamIO doesn't set createcnt?
-            // Actually createcnt is in IMAGE struct, so it was copied.
-
-            imagearray[ID].used = 1; // next_avail_image_ID sets this, but just to be sure if we used different logic
-
-            updateIMGIDcreationparams(img);
-        }
-        else
-        {
-            // No space available
-            if (img->im != NULL)
-            {
-                ImageStreamIO_closeIm(img->im);
-                free(img->im);
-            }
-            img->ID = -1;
-        }
-    }
-
-    return ID;
-}
 
 
 imageID read_sharedmem_image(
