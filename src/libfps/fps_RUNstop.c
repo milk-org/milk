61afc7e6c7182fb6746e068f9371357834e26556
oguyon <oliv.guyon@gmail.com>
2026-03-07 06:42:11 -1000

    feat: Implement conditional `fpsexec` for FPS start/stop commands, enhance `milk-fps-list` with process info status, and refine TUI display for FPS controls.

--- src/libfps/fps_RUNstop.c
+++ src/libfps/fps_RUNstop.c
@@ -19,8 +19,15 @@ errno_t functionparameter_RUNstop(FUNCTION_PARAMETER_STRUCT *fps)
                            fps->md->name,
                            fps->md->workdir);
 
-    EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:ctrl \" fpsrunstop\" C-m",
-                           fps->md->name);
+    if (strstr(fps->md->execfullpath, "fpsexec") != NULL) {
+        EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:ctrl \" %s %s:runstop\" C-m",
+                               fps->md->name,
+                               fps->md->execfullpath,
+                               fps->md->name);
+    } else {
+        EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:ctrl \" fpsrunstop\" C-m",
+                               fps->md->name);
+    }
 
     // Send C-c in case runstop command is not implemented
     EXECUTE_SYSTEM_COMMAND("tmux send-keys -t %s:run C-c &> /dev/null",
