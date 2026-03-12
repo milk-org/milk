7f21f53396be35ba914607d87f416120eeabaf96
oguyon <oliv.guyon@gmail.com>
2026-03-11 16:29:15 -1000

    Migrate compute_SVDpseudoInverse to IMGID API\n\n- Input via resolveIMGID, VT matrix and C matrix outputs via IMGID\n  with explicit mdt setup for datatype preservation\n- Test-mode images (AtA, M2) also converted\n- Eliminated arraysizetmp malloc, all dcimg[] replaced"

--- plugins/milk-extra-src/linopt_imtools/compute_SVDpseudoInverse.c
+++ plugins/milk-extra-src/linopt_imtools/compute_SVDpseudoInverse.c
@@ -101,9 +101,6 @@ linopt_compute_SVDpseudoInverse(
     FILE    *fp;
     char     fname[200];
     long     m, n;
-    imageID  ID_Rmatrix, ID_Cmatrix;
-    imageID  ID_VTmatrix;
-    uint32_t *arraysizetmp;
     double   egvlim;
     long     nbmodesremoved;
     uint8_t  datatype;
@@ -116,317 +113,424 @@ linopt_compute_SVDpseudoInverse(
     double          t45d, t56d, t67d;
     struct timespec tdiff;
 
-    int     testmode = 0;
-    imageID ID_AtA, ID;
+    int testmode = 0;
 
     printf("[CPU (lapack) SVD start]");
     fflush(stdout);
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t0);
     }
 
-    arraysizetmp =
-        (uint32_t *) malloc(sizeof(uint32_t) * 3);
-    if (arraysizetmp == NULL) {
-        FUNC_RETURN_FAILURE(
-            "malloc returns NULL pointer");
-    }
+    IMGID imgin =
+        imgid_make_from_name(
+            ID_Rmatrix_name);
+    resolveIMGID(&imgin,
+                 ERRMODE_ABORT,
+                 dcimg, dcnimg);
 
-    ID_Rmatrix =
-        image_ID(ID_Rmatrix_name, dcimg, dcnimg);
-    if (ID_Rmatrix == -1) {
-        printf("ERROR: matrix %s not found\n",
-               ID_Rmatrix_name);
-        exit(0);
+    datatype = imgin.md->datatype;
+    if(imgin.md->naxis == 3)
+    {
+        n = imgin.md->size[0]
+            * imgin.md->size[1];
+        m = imgin.md->size[2];
+        printf(
+            "3D image -> %ld %ld\n",
+            n, m);
     }
-    datatype = dcimg[ID_Rmatrix].md[0].datatype;
-    if (dcimg[ID_Rmatrix].md[0].naxis == 3) {
-        n = dcimg[ID_Rmatrix].md[0].size[0]
-            * dcimg[ID_Rmatrix].md[0].size[1];
-        m = dcimg[ID_Rmatrix].md[0].size[2];
-        printf("3D image -> %ld %ld\n", n, m);
-    } else {
-        n = dcimg[ID_Rmatrix].md[0].size[0];
-        m = dcimg[ID_Rmatrix].md[0].size[1];
-        printf("2D image -> %ld %ld\n", n, m);
+    else
+    {
+        n = imgin.md->size[0];
+        m = imgin.md->size[1];
+        printf(
+            "2D image -> %ld %ld\n",
+            n, m);
     }
     fflush(stdout);
 
-    printf("m = %ld , n = %ld \n", m, n);
+    printf(
+        "m = %ld , n = %ld \n",
+        m, n);
     fflush(stdout);
 
     /* Allocate double work arrays */
     double *D =
-        calloc((size_t) n * m, sizeof(double));
+        calloc((size_t) n * m,
+               sizeof(double));
     double *Ds =
-        calloc((size_t) m * n, sizeof(double));
+        calloc((size_t) m * n,
+               sizeof(double));
     double *DtD =
-        calloc((size_t) m * m, sizeof(double));
+        calloc((size_t) m * m,
+               sizeof(double));
     double *DtDinv =
-        calloc((size_t) m * m, sizeof(double));
+        calloc((size_t) m * m,
+               sizeof(double));
     double *eval =
-        calloc((size_t) m, sizeof(double));
+        calloc((size_t) m,
+               sizeof(double));
     double *tmp1 =
-        calloc((size_t) m * m, sizeof(double));
+        calloc((size_t) m * m,
+               sizeof(double));
     double *tmp2 =
-        calloc((size_t) m * m, sizeof(double));
+        calloc((size_t) m * m,
+               sizeof(double));
 
-    /* Fill D column-major: D[ii + k*n] */
-    if (datatype == _DATATYPE_FLOAT) {
-        for (long k = 0; k < m; k++) {
-            for (long ii = 0; ii < n; ii++) {
+    /* Fill D column-major */
+    if(datatype == _DATATYPE_FLOAT)
+    {
+        for(long k = 0; k < m; k++)
+        {
+            for(long ii = 0;
+                ii < n; ii++)
+            {
                 D[ii + k * n] =
-                    dcimg[ID_Rmatrix].array.F[
+                    imgin.im->array.F[
                         k * n + ii];
             }
         }
-    } else {
-        for (long k = 0; k < m; k++) {
-            for (long ii = 0; ii < n; ii++) {
+    }
+    else
+    {
+        for(long k = 0; k < m; k++)
+        {
+            for(long ii = 0;
+                ii < n; ii++)
+            {
                 D[ii + k * n] =
-                    dcimg[ID_Rmatrix].array.D[
+                    imgin.im->array.D[
                         k * n + ii];
             }
         }
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t1);
     }
 
     /* DtD = D^T * D  (m x m) */
-    cblas_dgemm(CblasColMajor,
-                CblasTrans, CblasNoTrans,
-                (int) m, (int) m, (int) n,
-                1.0, D, (int) n,
-                D, (int) n,
-                0.0, DtD, (int) m);
+    cblas_dgemm(
+        CblasColMajor,
+        CblasTrans, CblasNoTrans,
+        (int) m, (int) m, (int) n,
+        1.0, D, (int) n,
+        D, (int) n,
+        0.0, DtD, (int) m);
 
-    if (testmode == 1) {
-        FUNC_CHECK_RETURN(
-            create_2Dimage_ID("AtA", m, m,
-                              &ID_AtA));
-        for (long ii = 0; ii < m; ii++) {
-            for (long jj = 0; jj < m; jj++) {
-                dcimg[ID_AtA].array.F[
+    if(testmode == 1)
+    {
+        IMGID imgAtA =
+            imgid_make_from_name_2D(
+                "AtA", m, m);
+        imgAtA.mdt->shared = 0;
+        imgAtA.im = (IMAGE *) calloc(
+            1, sizeof(IMAGE));
+        imgid_mkimage(&imgAtA);
+
+        for(long ii = 0;
+            ii < m; ii++)
+        {
+            for(long jj = 0;
+                jj < m; jj++)
+            {
+                imgAtA.im->array.F[
                     jj * m + ii] =
-                    (float) DtD[ii + jj * m];
+                    (float) DtD[
+                        ii + jj * m];
             }
         }
-        save_fits("AtA", "test_AtA.fits");
+        save_fits(
+            "AtA", "test_AtA.fits");
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t2);
     }
 
-    /* Eigenvalue decomposition of DtD.
-     * After dsyev, DtD columns = eigenvectors,
-     * eval = eigenvalues in ascending order. */
+    /* Eigenvalue decomposition */
     {
-        /* dsyev destroys input, so work on DtD */
         int info = LAPACKE_dsyev(
             LAPACK_COL_MAJOR, 'V', 'U',
-            (int) m, DtD, (int) m, eval);
-        if (info != 0) {
+            (int) m, DtD,
+            (int) m, eval);
+        if(info != 0)
+        {
             printf(
-                "LAPACKE_dsyev failed: %d\n",
+                "LAPACKE_dsyev"
+                " failed: %d\n",
                 info);
         }
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t3);
     }
 
     /* Reverse to descending order */
-    for (long i = 0; i < m / 2; i++) {
+    for(long i = 0; i < m / 2; i++)
+    {
         double t = eval[i];
         eval[i] = eval[m - 1 - i];
         eval[m - 1 - i] = t;
     }
-    for (long i = 0; i < m / 2; i++) {
-        for (long j = 0; j < m; j++) {
-            double t = DtD[j + i * m];
+    for(long i = 0; i < m / 2; i++)
+    {
+        for(long j = 0; j < m; j++)
+        {
+            double t =
+                DtD[j + i * m];
             DtD[j + i * m] =
-                DtD[j + (m - 1 - i) * m];
-            DtD[j + (m - 1 - i) * m] = t;
+                DtD[j
+                    + (m - 1 - i) * m];
+            DtD[j
+                + (m - 1 - i) * m]
+                = t;
         }
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t4);
     }
 
     /* Write eigenvalues to file */
     sprintf(fname, "eigenv.dat");
-    if ((fp = fopen(fname, "w")) == NULL) {
-        printf("ERROR: cannot create \"%s\"\n",
-               fname);
+    fp = fopen(fname, "w");
+    if(fp == NULL)
+    {
+        printf(
+            "ERROR: cannot create"
+            " \"%s\"\n",
+            fname);
         exit(0);
     }
-    for (long k = 0; k < m; k++) {
+    for(long k = 0; k < m; k++)
+    {
         fprintf(fp, "%ld %g %g\n",
-                k, sqrt(eval[k]), eval[k]);
+                k, sqrt(eval[k]),
+                eval[k]);
     }
     fclose(fp);
 
-    egvlim = SVDeps * SVDeps * eval[0];
+    egvlim = SVDeps * SVDeps
+             * eval[0];
     MaxNBmodes1 = MaxNBmodes;
-    if (MaxNBmodes1 > m) {
+    if(MaxNBmodes1 > m)
+    {
         MaxNBmodes1 = m;
     }
-    if (MaxNBmodes1 > n) {
+    if(MaxNBmodes1 > n)
+    {
         MaxNBmodes1 = n;
     }
     mode = 0;
-    while ((mode < MaxNBmodes1)
-           && (eval[mode] > egvlim))
+    while((mode < MaxNBmodes1)
+          && (eval[mode] > egvlim))
     {
         mode++;
     }
-    printf("Keeping %ld modes  "
-           "(SVDeps = %g-> %g, "
-           "MaxNBmodes = %ld -> %ld)\n",
-           mode, SVDeps, egvlim,
-           MaxNBmodes, MaxNBmodes1);
+    printf(
+        "Keeping %ld modes  "
+        "(SVDeps = %g-> %g, "
+        "MaxNBmodes = %ld -> %ld)\n",
+        mode, SVDeps, egvlim,
+        MaxNBmodes, MaxNBmodes1);
     MaxNBmodes1 = mode;
 
     /* Write rotation matrix VT */
-    arraysizetmp[0] = m;
-    arraysizetmp[1] = m;
+    IMGID imgVT =
+        imgid_make_from_name_2D(
+            ID_VTmatrix_name, m, m);
+    imgVT.mdt->datatype = datatype;
+    imgVT.mdt->shared = 0;
+    imgVT.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgVT);
 
-    FUNC_CHECK_RETURN(
-        create_image_ID(ID_VTmatrix_name,
-                        2, arraysizetmp,
-                        datatype,
-                        0, 0, 0,
-                        &ID_VTmatrix));
-
-    if (datatype == _DATATYPE_FLOAT) {
-        for (long ii = 0; ii < m; ii++) {
-            for (long k = 0; k < m; k++) {
-                dcimg[ID_VTmatrix].array.F[
+    if(datatype == _DATATYPE_FLOAT)
+    {
+        for(long ii = 0;
+            ii < m; ii++)
+        {
+            for(long k = 0;
+                k < m; k++)
+            {
+                imgVT.im->array.F[
                     k * m + ii] =
-                    (float) DtD[k + ii * m];
+                    (float) DtD[
+                        k + ii * m];
             }
         }
-    } else {
-        for (long ii = 0; ii < m; ii++) {
-            for (long k = 0; k < m; k++) {
-                dcimg[ID_VTmatrix].array.D[
+    }
+    else
+    {
+        for(long ii = 0;
+            ii < m; ii++)
+        {
+            for(long k = 0;
+                k < m; k++)
+            {
+                imgVT.im->array.D[
                     k * m + ii] =
                     DtD[k + ii * m];
             }
         }
     }
 
-    if (testmode == 1) {
+    if(testmode == 1)
+    {
         save_fits(ID_VTmatrix_name,
                   "test_VT.fits");
     }
 
-    /* Build diagonal inverse matrix (tmp1) */
+    /* Build diagonal inverse */
     nbmodesremoved = 0;
     memset(tmp1, 0,
-           (size_t) m * m * sizeof(double));
-    for (long ii = 0; ii < m; ii++) {
-        if (ii > MaxNBmodes1 - 1) {
+           (size_t) m * m
+           * sizeof(double));
+    for(long ii = 0; ii < m; ii++)
+    {
+        if(ii > MaxNBmodes1 - 1)
+        {
             nbmodesremoved++;
-        } else {
+        }
+        else
+        {
             tmp1[ii + ii * m] =
                 1.0 / eval[ii];
         }
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t5);
     }
 
-    /* DtDinv = evec * diag^-1 * evec^T */
-    cblas_dgemm(CblasColMajor,
-                CblasNoTrans, CblasNoTrans,
-                (int) m, (int) m, (int) m,
-                1.0, DtD, (int) m,
-                tmp1, (int) m,
-                0.0, tmp2, (int) m);
-    cblas_dgemm(CblasColMajor,
-                CblasNoTrans, CblasTrans,
-                (int) m, (int) m, (int) m,
-                1.0, tmp2, (int) m,
-                DtD, (int) m,
-                0.0, DtDinv, (int) m);
+    /* DtDinv = evec * diag^-1
+     * * evec^T */
+    cblas_dgemm(
+        CblasColMajor,
+        CblasNoTrans, CblasNoTrans,
+        (int) m, (int) m, (int) m,
+        1.0, DtD, (int) m,
+        tmp1, (int) m,
+        0.0, tmp2, (int) m);
+    cblas_dgemm(
+        CblasColMajor,
+        CblasNoTrans, CblasTrans,
+        (int) m, (int) m, (int) m,
+        1.0, tmp2, (int) m,
+        DtD, (int) m,
+        0.0, DtDinv, (int) m);
 
-    if (testmode == 1) {
-        FUNC_CHECK_RETURN(
-            create_2Dimage_ID("M2", m, m, &ID));
-        for (long ii = 0; ii < m; ii++) {
-            for (long jj = 0; jj < m; jj++) {
-                dcimg[ID].array.F[
+    if(testmode == 1)
+    {
+        IMGID imgM2 =
+            imgid_make_from_name_2D(
+                "M2", m, m);
+        imgM2.mdt->shared = 0;
+        imgM2.im = (IMAGE *) calloc(
+            1, sizeof(IMAGE));
+        imgid_mkimage(&imgM2);
+
+        for(long ii = 0;
+            ii < m; ii++)
+        {
+            for(long jj = 0;
+                jj < m; jj++)
+            {
+                imgM2.im->array.F[
                     jj * m + ii] =
                     (float) DtDinv[
                         ii + jj * m];
             }
         }
-        save_fits("M2", "test_M2.fits");
+        save_fits(
+            "M2", "test_M2.fits");
     }
 
     /* Ds = DtDinv * D^T  (m x n) */
-    cblas_dgemm(CblasColMajor,
-                CblasNoTrans, CblasTrans,
-                (int) m, (int) n, (int) m,
-                1.0, DtDinv, (int) m,
-                D, (int) n,
-                0.0, Ds, (int) m);
+    cblas_dgemm(
+        CblasColMajor,
+        CblasNoTrans, CblasTrans,
+        (int) m, (int) n, (int) m,
+        1.0, DtDinv, (int) m,
+        D, (int) n,
+        0.0, Ds, (int) m);
 
-    if (dcimg[ID_Rmatrix].md[0].naxis == 3) {
-        arraysizetmp[0] =
-            dcimg[ID_Rmatrix].md[0].size[0];
-        arraysizetmp[1] =
-            dcimg[ID_Rmatrix].md[0].size[1];
-        arraysizetmp[2] = m;
-    } else {
-        arraysizetmp[0] = n;
-        arraysizetmp[1] = m;
+    IMGID imgC =
+        imgid_make_from_name(
+            ID_Cmatrix_name);
+    if(imgin.md->naxis == 3)
+    {
+        imgC.mdt->naxis = 3;
+        imgC.mdt->size[0] =
+            imgin.md->size[0];
+        imgC.mdt->size[1] =
+            imgin.md->size[1];
+        imgC.mdt->size[2] = m;
     }
+    else
+    {
+        imgC.mdt->naxis = 2;
+        imgC.mdt->size[0] = n;
+        imgC.mdt->size[1] = m;
+    }
+    imgC.mdt->datatype = datatype;
+    imgC.mdt->shared = 0;
+    imgC.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgC);
 
-    FUNC_CHECK_RETURN(
-        create_image_ID(
-            ID_Cmatrix_name,
-            dcimg[ID_Rmatrix].md[0].naxis,
-            arraysizetmp, datatype,
-            0, 0, 0, &ID_Cmatrix));
-
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t6);
     }
 
     /* Write result */
-    if (datatype == _DATATYPE_FLOAT) {
-        for (long ii = 0; ii < n; ii++) {
-            for (long k = 0; k < m; k++) {
-                dcimg[ID_Cmatrix].array.F[
+    if(datatype == _DATATYPE_FLOAT)
+    {
+        for(long ii = 0;
+            ii < n; ii++)
+        {
+            for(long k = 0;
+                k < m; k++)
+            {
+                imgC.im->array.F[
                     k * n + ii] =
-                    (float) Ds[k + ii * m];
+                    (float) Ds[
+                        k + ii * m];
             }
         }
-    } else {
-        for (long ii = 0; ii < n; ii++) {
-            for (long k = 0; k < m; k++) {
-                dcimg[ID_Cmatrix].array.D[
+    }
+    else
+    {
+        for(long ii = 0;
+            ii < n; ii++)
+        {
+            for(long k = 0;
+                k < m; k++)
+            {
+                imgC.im->array.D[
                     k * n + ii] =
                     Ds[k + ii * m];
             }
         }
     }
 
-    if (testmode == 1) {
+    if(testmode == 1)
+    {
         save_fits(ID_Cmatrix_name,
                   "test_Ainv.fits");
     }
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         clock_gettime(CLOCK_MILK, &t7);
     }
 
@@ -437,53 +541,69 @@ linopt_compute_SVDpseudoInverse(
     free(DtDinv);
     free(tmp1);
     free(tmp2);
-    free(arraysizetmp);
 
-    printf("[CPU pseudo-inverse done]\n");
+    printf(
+        "[CPU pseudo-inverse done]\n");
     fflush(stdout);
 
-    if (timing == 1) {
+    if(timing == 1)
+    {
         tdiff = timespec_diff(t0, t1);
-        t01d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t01d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t1, t2);
-        t12d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t12d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t2, t3);
-        t23d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t23d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t3, t4);
-        t34d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t34d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t4, t5);
-        t45d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t45d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t5, t6);
-        t56d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t56d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
         tdiff = timespec_diff(t6, t7);
-        t67d  = 1.0 * tdiff.tv_sec
-                + 1.0e-9 * tdiff.tv_nsec;
+        t67d = 1.0 * tdiff.tv_sec
+               + 1.0e-9
+                 * tdiff.tv_nsec;
 
         printf("Timing info: \n");
-        printf("  0-1\t%12.3f ms\n",
-               t01d * 1000.0);
-        printf("  1-2\t%12.3f ms\n",
-               t12d * 1000.0);
-        printf("  2-3\t%12.3f ms\n",
-               t23d * 1000.0);
-        printf("  3-4\t%12.3f ms\n",
-               t34d * 1000.0);
-        printf("  4-5\t%12.3f ms\n",
-               t45d * 1000.0);
-        printf("  5-6\t%12.3f ms\n",
-               t56d * 1000.0);
-        printf("  6-7\t%12.3f ms\n",
-               t67d * 1000.0);
+        printf(
+            "  0-1\t%12.3f ms\n",
+            t01d * 1000.0);
+        printf(
+            "  1-2\t%12.3f ms\n",
+            t12d * 1000.0);
+        printf(
+            "  2-3\t%12.3f ms\n",
+            t23d * 1000.0);
+        printf(
+            "  3-4\t%12.3f ms\n",
+            t34d * 1000.0);
+        printf(
+            "  4-5\t%12.3f ms\n",
+            t45d * 1000.0);
+        printf(
+            "  5-6\t%12.3f ms\n",
+            t56d * 1000.0);
+        printf(
+            "  6-7\t%12.3f ms\n",
+            t67d * 1000.0);
     }
 
-    if (outID != NULL) {
-        *outID = ID_Cmatrix;
+    if(outID != NULL)
+    {
+        *outID = imgC.ID;
     }
 
     DEBUG_TRACE_FEXIT();
