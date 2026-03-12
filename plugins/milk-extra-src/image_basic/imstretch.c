d0e888c63b55a67f4485394ee0a5f8d8e097ff98
oguyon <oliv.guyon@gmail.com>
2026-03-11 16:22:11 -1000

    Migrate makeCosRadModes, imstretch, measure_transl to IMGID API\n\n- makeCosRadModes.c: temp 2D r-image + 3D output cube\n- imstretch.c: all 3 stretch functions (basic_stretch, range, stretchc)\n- measure_transl.c: 5 images, ~20 dcimg[] sites in multi-scale search"

--- plugins/milk-extra-src/image_basic/imstretch.c
+++ plugins/milk-extra-src/image_basic/imstretch.c
@@ -17,82 +17,135 @@
 #include "COREMOD_arith/COREMOD_arith.h"
 #include "COREMOD_memory/COREMOD_memory.h"
 
-imageID basic_stretch(const char *__restrict name_in,
-                      const char *__restrict name_out,
-                      float coeff,
-                      long  Xcenter,
-                      long  Ycenter)
+/**
+ * Stretch image by coefficient
+ * around a center point.
+ */
+imageID basic_stretch(
+    const char *__restrict name_in,
+    const char *__restrict name_out,
+    float coeff,
+    long  Xcenter,
+    long  Ycenter)
 {
+    IMGID imgin =
+        imgid_make_from_name(name_in);
+    resolveIMGID(&imgin, ERRMODE_ABORT,
+                 dcimg, dcnimg);
+
     uint32_t naxes[2];
-    imageID  IDin;
-    imageID  IDout;
-    long     i, j;
+    naxes[0] = imgin.md->size[0];
+    naxes[1] = imgin.md->size[1];
 
-    IDin     = image_ID(name_in, dcimg, dcnimg);
-    naxes[0] = dcimg[IDin].md[0].size[0];
-    naxes[1] = dcimg[IDin].md[0].size[1];
+    IMGID imgout =
+        imgid_make_from_name_2D(
+            name_out,
+            naxes[0], naxes[1]);
+    imgout.mdt->shared = 0;
+    imgout.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgout);
 
-    create_2Dimage_ID(name_out, naxes[0], naxes[1], &IDout);
-
-    for(uint32_t ii = 0; ii < naxes[0]; ii++)
-        for(uint32_t jj = 0; jj < naxes[0]; jj++)
+    for(uint32_t ii = 0;
+        ii < naxes[0]; ii++)
+    {
+        for(uint32_t jj = 0;
+            jj < naxes[0]; jj++)
         {
-            i = Xcenter + (long)(1.0 * (ii - Xcenter) * coeff);
-            j = Ycenter + (long)(1.0 * (jj - Ycenter) * coeff);
-            if((i < naxes[0]) && (j < naxes[1]) && (i > -1) && (j > -1))
+            long i = Xcenter
+                     + (long)(1.0
+                              * (ii
+                                 - Xcenter)
+                              * coeff);
+            long j = Ycenter
+                     + (long)(1.0
+                              * (jj
+                                 - Ycenter)
+                              * coeff);
+            if((i < naxes[0])
+               && (j < naxes[1])
+               && (i > -1)
+               && (j > -1))
             {
-                dcimg[IDout].array.F[jj * naxes[0] + ii] +=
-                    dcimg[IDin].array.F[j * naxes[0] + i] / coeff / coeff;
+                imgout.im->array.F[
+                    jj * naxes[0] + ii]
+                    += imgin.im->array.F[
+                           j * naxes[0]
+                           + i]
+                       / coeff / coeff;
             }
         }
+    }
 
-    arith_image_cstmult_inplace(name_out,
-                                arith_image_total(name_in) /
-                                arith_image_total(name_out));
+    arith_image_cstmult_inplace(
+        name_out,
+        arith_image_total(name_in)
+        / arith_image_total(name_out));
 
-    return IDout;
+    return imgout.ID;
 }
 
-imageID basic_stretch_range(const char *__restrict name_in,
-                            const char *__restrict name_out,
-                            float coeff1,
-                            float coeff2,
-                            long  Xcenter,
-                            long  Ycenter,
-                            long  NBstep,
-                            float ApoCoeff)
+/**
+ * Stretch image with multi-step range
+ * of coefficients and apodization.
+ */
+imageID basic_stretch_range(
+    const char *__restrict name_in,
+    const char *__restrict name_out,
+    float coeff1,
+    float coeff2,
+    long  Xcenter,
+    long  Ycenter,
+    long  NBstep,
+    float ApoCoeff)
 {
     DEBUG_TRACE_FSTART();
 
-    // ApoCoeff should be between 0 and 1
+    float eps = 1.0e-5;
+
+    IMGID imgin =
+        imgid_make_from_name(name_in);
+    resolveIMGID(&imgin, ERRMODE_ABORT,
+                 dcimg, dcnimg);
+
     uint32_t naxes[2];
-    imageID  IDin, IDout;
-    long     i, j;
-    float    coeff;
-    long     step;
-    float    mcoeff;
-    float    x, y;
-    float    eps = 1.0e-5;
-    float    u, t, tmp;
+    naxes[0] = imgin.md->size[0];
+    naxes[1] = imgin.md->size[1];
 
-    IDin     = image_ID(name_in, dcimg, dcnimg);
-    naxes[0] = dcimg[IDin].md[0].size[0];
-    naxes[1] = dcimg[IDin].md[0].size[1];
+    IMGID imgout =
+        imgid_make_from_name_2D(
+            name_out,
+            naxes[0], naxes[1]);
+    imgout.mdt->shared = 0;
+    imgout.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgout);
 
-    FUNC_CHECK_RETURN(create_2Dimage_ID(name_out, naxes[0], naxes[1], &IDout));
-
-    for(step = 0; step < NBstep; step++)
+    for(long step = 0;
+        step < NBstep; step++)
     {
         fprintf(stdout, ".");
         fflush(stdout);
-        coeff = coeff1 + (coeff2 - coeff1) * (1.0 * step / (NBstep - 1));
-        x     = (coeff - (coeff1 + coeff2) / 2.0) / ((coeff2 - coeff1) / 2.0);
-        // x goes from -1 to 1
+        float coeff =
+            coeff1
+            + (coeff2 - coeff1)
+              * (1.0 * step
+                 / (NBstep - 1));
+        float x =
+            (coeff
+             - (coeff1 + coeff2) / 2.0)
+            / ((coeff2 - coeff1) / 2.0);
+        float mcoeff;
         if(ApoCoeff > eps)
         {
-            mcoeff =
-                pow((1.0 - pow((fabs(x) - (1.0 - ApoCoeff)) / ApoCoeff, 2.0)),
-                    4.0);
+            mcoeff = pow(
+                (1.0
+                 - pow(
+                     (fabs(x)
+                      - (1.0 - ApoCoeff))
+                     / ApoCoeff,
+                     2.0)),
+                4.0);
         }
         else
         {
@@ -107,77 +160,145 @@ imageID basic_stretch_range(const char *__restrict name_in,
         {
             mcoeff = 1.0;
         }
-        //      fprintf(stdout,"(%f %f %f %f %f)",coeff,coeff1,coeff2,x,mcoeff);
 
-        for(uint32_t ii = 0; ii < naxes[0]; ii++)
-            for(uint32_t jj = 0; jj < naxes[1]; jj++)
+        for(uint32_t ii = 0;
+            ii < naxes[0]; ii++)
+        {
+            for(uint32_t jj = 0;
+                jj < naxes[1]; jj++)
             {
-                x = (1.0 * (ii - Xcenter) * coeff) + Xcenter;
-                y = (1.0 * (jj - Ycenter) * coeff) + Ycenter;
-                i = (long) x;
-                j = (long) y;
-                u = x - i;
-                t = y - j;
-                if((i < naxes[0] - 1) && (j < naxes[1] - 1) && (i > -1) &&
-                        (j > -1))
+                x = (1.0
+                     * (ii - Xcenter)
+                     * coeff)
+                    + Xcenter;
+                float y =
+                    (1.0
+                     * (jj - Ycenter)
+                     * coeff)
+                    + Ycenter;
+                long i = (long) x;
+                long j = (long) y;
+                float u = x - i;
+                float t = y - j;
+                if((i < naxes[0] - 1)
+                   && (j < naxes[1] - 1)
+                   && (i > -1)
+                   && (j > -1))
                 {
-                    tmp = (1.0 - u) * (1.0 - t) *
-                          dcimg[IDin].array.F[j * naxes[0] + i];
-                    tmp += (1.0 - u) * t *
-                           dcimg[IDin].array.F[(j + 1) * naxes[0] + i];
-                    tmp += u * (1.0 - t) *
-                           dcimg[IDin].array.F[j * naxes[0] + i + 1];
-                    tmp += u * t *
-                           dcimg[IDin].array.F[(j + 1) * naxes[0] + i + 1];
-                    dcimg[IDout].array.F[jj * naxes[0] + ii] +=
-                        mcoeff * tmp / coeff / coeff;
+                    float tmp =
+                        (1.0 - u)
+                        * (1.0 - t)
+                        * imgin.im
+                              ->array.F[
+                                  j
+                                  * naxes[0]
+                                  + i];
+                    tmp +=
+                        (1.0 - u) * t
+                        * imgin.im
+                              ->array.F[
+                                  (j + 1)
+                                  * naxes[0]
+                                  + i];
+                    tmp +=
+                        u * (1.0 - t)
+                        * imgin.im
+                              ->array.F[
+                                  j
+                                  * naxes[0]
+                                  + i
+                                  + 1];
+                    tmp +=
+                        u * t
+                        * imgin.im
+                              ->array.F[
+                                  (j + 1)
+                                  * naxes[0]
+                                  + i
+                                  + 1];
+                    imgout.im->array.F[
+                        jj * naxes[0]
+                        + ii]
+                        += mcoeff * tmp
+                           / coeff
+                           / coeff;
                 }
             }
+        }
     }
 
     fprintf(stdout, "\n");
-    arith_image_cstmult_inplace(name_out,
-                                arith_image_total(name_in) /
-                                arith_image_total(name_out));
+    arith_image_cstmult_inplace(
+        name_out,
+        arith_image_total(name_in)
+        / arith_image_total(name_out));
 
     DEBUG_TRACE_FEXIT();
-    return IDout;
+    return imgout.ID;
 }
 
-imageID basic_stretchc(const char *__restrict name_in,
-                       const char *__restrict name_out,
-                       float coeff)
+/**
+ * Stretch image by coefficient around
+ * center of image.
+ */
+imageID basic_stretchc(
+    const char *__restrict name_in,
+    const char *__restrict name_out,
+    float coeff)
 {
     DEBUG_TRACE_FSTART();
 
+    IMGID imgin =
+        imgid_make_from_name(name_in);
+    resolveIMGID(&imgin, ERRMODE_ABORT,
+                 dcimg, dcnimg);
+
     uint32_t naxes[2];
-    imageID  IDin;
-    imageID  IDout;
-    long     i, j;
-    long     Xcenter, Ycenter;
+    naxes[0] = imgin.md->size[0];
+    naxes[1] = imgin.md->size[1];
+    long Xcenter = naxes[0] / 2;
+    long Ycenter = naxes[1] / 2;
 
-    IDin     = image_ID(name_in, dcimg, dcnimg);
-    naxes[0] = dcimg[IDin].md[0].size[0];
-    naxes[1] = dcimg[IDin].md[0].size[1];
-    Xcenter  = naxes[0] / 2;
-    Ycenter  = naxes[1] / 2;
+    IMGID imgout =
+        imgid_make_from_name_2D(
+            name_out,
+            naxes[0], naxes[1]);
+    imgout.mdt->shared = 0;
+    imgout.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgout);
 
-    FUNC_CHECK_RETURN(create_2Dimage_ID(name_out, naxes[0], naxes[1], &IDout));
-
-    for(uint32_t ii = 0; ii < naxes[0]; ii++)
-        for(uint32_t jj = 0; jj < naxes[0]; jj++)
+    for(uint32_t ii = 0;
+        ii < naxes[0]; ii++)
+    {
+        for(uint32_t jj = 0;
+            jj < naxes[0]; jj++)
         {
-            i = Xcenter + (long)(1.0 * (ii - Xcenter) * coeff);
-            j = Ycenter + (long)(1.0 * (jj - Ycenter) * coeff);
-            if((i < naxes[0]) && (j < naxes[1]) && (i > -1) && (j > -1))
+            long i = Xcenter
+                     + (long)(1.0
+                              * (ii
+                                 - Xcenter)
+                              * coeff);
+            long j = Ycenter
+                     + (long)(1.0
+                              * (jj
+                                 - Ycenter)
+                              * coeff);
+            if((i < naxes[0])
+               && (j < naxes[1])
+               && (i > -1)
+               && (j > -1))
             {
-                dcimg[IDout].array.F[jj * naxes[0] + ii] +=
-                    dcimg[IDin].array.F[j * naxes[0] + i] / coeff / coeff;
+                imgout.im->array.F[
+                    jj * naxes[0] + ii]
+                    += imgin.im->array.F[
+                           j * naxes[0]
+                           + i]
+                       / coeff / coeff;
             }
         }
-
-    /*  basic_mult(name_out,arith_image_total(name_in)/arith_image_total(name_out));*/
+    }
 
     DEBUG_TRACE_FEXIT();
-    return IDout;
+    return imgout.ID;
 }
