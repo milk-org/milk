7a768da9cefe37905c380cbdbd0a4306ffa15528
oguyon <oliv.guyon@gmail.com>
2026-03-11 16:06:57 -1000

    Migrate streamrecord, extrapolate, loadfitsimgcube, readPGM, read_binary32f to IMGID\n\n- Replace image_ID + dcimg[] with resolveIMGID + im->array\n- Replace create_2Dimage_ID/create_3Dimage_ID with imgid_make_from_name_2D/3D + imgid_mkimage\n- Remove unused variable declarations\n- Add Kernel-Doc comments"

--- plugins/milk-extra-src/image_basic/loadfitsimgcube.c
+++ plugins/milk-extra-src/image_basic/loadfitsimgcube.c
@@ -92,11 +92,14 @@ CLIADDCMD_image_basic__loadfitsimgcube()
     return RETURN_SUCCESS;
 }
 
-// load all images matching strfilter + .fits into a data cube
-// return number of images loaded
-// image name in buffer is same as file name without extension
-long load_fitsimages_cube(const char *__restrict strfilter,
-                          const char *__restrict ID_out_name)
+/**
+ * Load all images matching strfilter
+ * into a single 3D data cube.
+ * Returns number of images loaded.
+ */
+long load_fitsimages_cube(
+    const char *__restrict strfilter,
+    const char *__restrict ID_out_name)
 {
     long     cnt = 0;
     char     fname[STRINGMAXLEN_FILENAME];
@@ -104,91 +107,124 @@ long load_fitsimages_cube(const char *__restrict strfilter,
     FILE    *fp;
     uint32_t xsize, ysize;
     imageID  ID;
-    imageID  IDout;
 
     printf("Filter = %s\n", strfilter);
 
-    EXECUTE_SYSTEM_COMMAND("ls %s > flist.tmp\n", strfilter);
+    EXECUTE_SYSTEM_COMMAND(
+        "ls %s > flist.tmp\n", strfilter);
 
     xsize = 0;
     ysize = 0;
 
-    if((fp = fopen("flist.tmp", "r")) == NULL)
+    if((fp = fopen("flist.tmp", "r"))
+       == NULL)
     {
         C_ERRNO = errno;
         PRINT_ERROR("fopen() error");
         exit(0);
     }
 
-    while(fgets(fname, STRINGMAXLEN_FILENAME, fp) != NULL)
+    while(fgets(fname,
+                STRINGMAXLEN_FILENAME,
+                fp) != NULL)
     {
         fname[strlen(fname) - 1] = '\0';
         if(cnt == 0)
         {
-            load_fits(fname, "imtmplfc", 1, &ID);
-            xsize = dcimg[ID].md[0].size[0];
-            ysize = dcimg[ID].md[0].size[1];
-            delete_image_ID("imtmplfc", DELETE_IMAGE_ERRMODE_WARNING);
+            load_fits(
+                fname,
+                "imtmplfc", 1, &ID);
+            xsize =
+                dcimg[ID].md[0].size[0];
+            ysize =
+                dcimg[ID].md[0].size[1];
+            delete_image_ID(
+                "imtmplfc",
+                DELETE_IMAGE_ERRMODE_WARNING);
         }
 
-        load_fits(fname, "imtmplfc", 1, &ID);
-        if((dcimg[ID].md[0].size[0] != xsize) ||
-                (dcimg[ID].md[0].size[1] != ysize))
+        load_fits(
+            fname, "imtmplfc", 1, &ID);
+        if((dcimg[ID].md[0].size[0]
+            != xsize)
+           || (dcimg[ID].md[0].size[1]
+               != ysize))
         {
             fprintf(stderr,
-                    "ERROR in load_fitsimages_cube: not all images have the "
-                    "same size\n");
+                    "ERROR in "
+                    "load_fitsimages_cube:"
+                    " not all images have"
+                    " the same size\n");
             exit(0);
         }
-        delete_image_ID("imtmplfc", DELETE_IMAGE_ERRMODE_WARNING);
+        delete_image_ID(
+            "imtmplfc",
+            DELETE_IMAGE_ERRMODE_WARNING);
         cnt++;
     }
     fclose(fp);
 
     printf("Creating 3D cube ... ");
     fflush(stdout);
-    create_3Dimage_ID(ID_out_name, xsize, ysize, cnt, &IDout);
+
+    IMGID imgout =
+        imgid_make_from_name_3D(
+            ID_out_name,
+            xsize, ysize, cnt);
+    imgout.mdt->shared = 0;
+    imgout.im = (IMAGE *) calloc(
+        1, sizeof(IMAGE));
+    imgid_mkimage(&imgout);
     printf("\n");
     fflush(stdout);
 
     cnt = 0;
-    if((fp = fopen("flist.tmp", "r")) == NULL)
+    if((fp = fopen("flist.tmp", "r"))
+       == NULL)
     {
         C_ERRNO = errno;
         PRINT_ERROR("fopen() error");
         exit(0);
     }
 
-    while(fgets(fname, STRINGMAXLEN_FILENAME, fp) != NULL)
+    while(fgets(fname,
+                STRINGMAXLEN_FILENAME,
+                fp) != NULL)
     {
         fname[strlen(fname) - 1] = '\0';
-        strncpy(fname1, fname, STRINGMAXLEN_FILENAME);
+        strncpy(fname1, fname,
+                STRINGMAXLEN_FILENAME);
         fname1[strlen(fname) - 5] = '\0';
-        load_fits(fname, fname1, 1, NULL);
-        printf("Image %s loaded -> %s\n", fname, fname1);
-        ID = image_ID(fname1, dcimg, dcnimg);
-        for(uint64_t ii = 0; ii < xsize * ysize; ii++)
+        load_fits(
+            fname, fname1, 1, NULL);
+        printf("Image %s loaded -> %s\n",
+               fname, fname1);
+
+        IMGID imgtmp =
+            imgid_make_from_name(fname1);
+        resolveIMGID(&imgtmp,
+                     ERRMODE_ABORT,
+                     dcimg, dcnimg);
+
+        for(uint64_t ii = 0;
+            ii < xsize * ysize; ii++)
         {
-            dcimg[IDout].array.F[xsize * ysize * cnt + ii] =
-                dcimg[ID].array.F[ii];
+            imgout.im->array.F[
+                xsize * ysize * cnt
+                + ii] =
+                imgtmp.im->array.F[ii];
         }
-        delete_image_ID(fname1, DELETE_IMAGE_ERRMODE_WARNING);
+        delete_image_ID(
+            fname1,
+            DELETE_IMAGE_ERRMODE_WARNING);
         cnt++;
     }
 
     fclose(fp);
-    /*  n = snprintf(command,SBUFFERSIZE,"rm flist.tmp");
-      if(n >= SBUFFERSIZE)
-          PRINT_ERROR("Attempted to write string buffer with too many characters");
 
-      if(system(command)==-1)
-      {
-          printf("WARNING: system(\"%s\") failed [function: %s  file: %s  line: %d ]\n",command,__func__,__FILE__,__LINE__);
-          //exit(0);
-      }
+    printf("%ld images loaded into "
+           "cube %s\n",
+           cnt, ID_out_name);
 
-    */
-    printf("%ld images loaded into cube %s\n", cnt, ID_out_name);
-
-    return (cnt);
+    return cnt;
 }
