91bd7eb143bafab24c2058c7f91fd321ba8f66f7
Olivier Guyon <oliv.guyon@gmail.com>
2026-03-13 19:24:12 -1000

    feat(cli): CLI improvements rounds 1 & 2 (#63)
    Aliases, watch, filepath completion, milkrc, timing, chaining, pipe, redirect, cmdstats, syntax highlighting, and updated docs.

--- src/cli/CLIcore/CLIcore.c
+++ src/cli/CLIcore/CLIcore.c
@@ -500,6 +500,15 @@ errno_t runCLI(int argc, char *argv[], char *promptstring)
     // CLI initialize
     CLI_startup();
 
+    // Load persistent command aliases
+    cli_alias_load();
+
+    // Enable syntax highlighting by default
+    data.syntax_highlight = 1;
+
+    // Load startup script (~/.milkrc)
+    cli_milkrc_load();
+
     // set shared memory directory
     setSHMdir();
 
@@ -1130,6 +1139,65 @@ void runCLI_cmd_init()
                        "<us>",
                        "usleep 1000",
                        "usleep(long tus)");
+
+    RegisterCLIcommand("alias",
+                       __FILE__,
+                       cli_alias_add,
+                       "create/update command alias",
+                       "<name> <command...>",
+                       "alias ld mem.listim",
+                       "cli_alias_add()");
+
+    RegisterCLIcommand("unalias",
+                       __FILE__,
+                       cli_alias_remove,
+                       "remove command alias",
+                       "<name>",
+                       "unalias ld",
+                       "cli_alias_remove()");
+
+    RegisterCLIcommand("aliases",
+                       __FILE__,
+                       cli_alias_list,
+                       "list all command aliases",
+                       "no argument",
+                       "aliases",
+                       "cli_alias_list()");
+
+    RegisterCLIcommand("watch",
+                       __FILE__,
+                       cli_watch,
+                       "repeat command at interval",
+                       "<interval_ms> <command...>",
+                       "watch 1000 mem.listim",
+                       "cli_watch()");
+
+    RegisterCLIcommand("time",
+                       __FILE__,
+                       cli_time,
+                       "measure command execution time",
+                       "<command...>",
+                       "time mem.listim",
+                       "cli_time()");
+
+    RegisterCLIcommand("cmdstats",
+                       __FILE__,
+                       cli_cmdstats,
+                       "show command usage statistics",
+                       "no argument",
+                       "cmdstats",
+                       "cli_cmdstats()");
+
+#ifdef USE_READLINE
+    RegisterCLIcommand(
+        "synhl",
+        __FILE__,
+        cli_syntax_highlight_toggle,
+        "toggle syntax highlighting",
+        "[on|off]",
+        "synhl off",
+        "cli_syntax_highlight_toggle()");
+#endif
 
     //  init_modules();
 
