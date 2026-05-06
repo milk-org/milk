ede77ec03a59872ac6723307bbe76acf7b36d4bd
oguyon <oliv.guyon@gmail.com>
2026-05-06 01:25:21 -1000

    chore: remove ghost submodule entries, add demo pipeline

--- plugins/milk-extra-src/linopt_imtools/makeCPAmodes.h
+++ plugins/milk-extra-src/linopt_imtools/makeCPAmodes.h
@@ -8,19 +8,26 @@
 
 errno_t CLIADDCMD_linopt_imtools__makeCPAmodes();
 
-errno_t linopt_imtools_makeCPAmodes(IMGID *imgoutm,
-                                    long        size,
-                                    float       rCPAmin,
-                                    float       rCPAmax,
-                                    float       CPAmax,
-                                    float       deltaCPA,
-                                    float       radius,
-                                    float       radfactlim,
-                                    int         writeMfile,
-                                    long       *outNBmax,
-                                    IMGID       imgmask,
-                                    float       extrfactor,
-                                    float       extroffset
-                                   );
+errno_t linopt_imtools_makeCPAmodes(
+    IMGID          *imgoutm,
+    uint32_t        sizex,
+    uint32_t        sizey,
+    float           xcenter,
+    float           ycenter,
+    float           rCPAmin,
+    float           rCPAmax,
+    float           CPAmax,
+    float           deltaCPA,
+    float           radius,
+    float           radfactlim,
+    float           fpowerlaw,
+    float           fpowerlaw_minf,
+    float           fpowerlaw_maxf,
+    uint32_t        writeMfile,
+    long           *outNBmax,
+    IMGID           imgmask,
+    float           extrfactor,
+    float           extroffset
+);
 
 #endif
