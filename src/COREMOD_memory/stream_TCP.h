cf041a33c6552e39f330932412b7dfd63594adca
vdeo <vdeo@naoj.org>
2026-05-12 13:23:46 +0200

    Refactor TCP transmit senders (not tested yet)

--- src/COREMOD_memory/stream_TCP.h
+++ src/COREMOD_memory/stream_TCP.h
@@ -5,19 +5,28 @@
 #ifndef _STREAM_TCP_H
 #define _STREAM_TCP_H
 
-errno_t stream__TCP_addCLIcmd();
+errno_t stream__NETW_addCLIcmd();
 
 errno_t COREMOD_MEMORY_testfunction_semaphore(const char *IDname,
-        int         semtrig,
-        int         testmode);
+        int semtrig,
+        int testmode);
 
 imageID COREMOD_MEMORY_image_NETWORKtransmit(const char *IDname,
         const char *IPaddr,
-        int         port,
-        int         mode,
-        int         RT_priority);
+        int port,
+        int mode,
+        int RT_priority);
 
-imageID
-COREMOD_MEMORY_image_NETWORKreceive(int port, int mode, int RT_priority);
+imageID COREMOD_MEMORY_image_NETWORKreceive(
+    int port, int mode, int RT_priority);
+
+imageID COREMOD_MEMORY_image_NETUDPtransmit(const char *IDname,
+        const char *IPaddr,
+        int port,
+        int mode,
+        int RT_priority);
+
+imageID COREMOD_MEMORY_image_NETUDPreceive(
+    int port, int mode, int RT_priority);
 
 #endif
