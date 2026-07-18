bdf6911032c50d8beb741a54227f6a6c4f0e736e
vdeo <vdeo@naoj.org>
2026-07-18 12:20:02 +0200

    Add a milk-common include file for BLAS and LAPACKE - plus a WEAK macro

---
+++ src/engine/libmilkcommon/milk_blas_lapacke.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_BLAS_LAPACKE_H
+#define INCLUDE_BLAS_LAPACKE_H
+
+// Provide BLAS from MKL or OPENBLAS
+#ifdef HAVE_MKL
+#    include "mkl.h"
+#else
+#    ifdef HAVE_OPENBLAS
+#        include <cblas.h>
+#    endif
+#endif
+
+// Provide LAPACKE from MKL or OPENBLAS or liblapacke
+#ifdef HAVE_LAPACKE
+#    ifdef HAVE_MKL
+#        include "mkl_lapacke.h"
+#    else
+#        include <lapacke.h> // May come from OPENBLAS or LAPACKE standalone
+#    endif
+#endif
+
+
+#endif // #ifndef INCLUDE_BLAS_LAPACKE_H
